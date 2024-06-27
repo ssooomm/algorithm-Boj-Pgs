426c2da956939e94070c31ad7df7f5f4089df13f
ssooomm <101492513+ssooomm@users.noreply.github.com>
2024-06-27 16:39:34 +0900

    [Silver II] Title: DNA 비밀번호, Time: 624 ms, Memory: 87104 KB -BaekjoonHub

---
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -0,0 +1,81 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int S = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+
+        String str = br.readLine();
+        st = new StringTokenizer(br.readLine());
+
+        int[] numArr = new int[4];
+        int nums = 0;
+        int answer = 0;
+
+        for (int i = 0; i < 4; i++) {
+            numArr[i] = Integer.parseInt(st.nextToken());
+            nums += numArr[i];
+        }
+
+        String[] strArr = str.split("");
+        int[] intArr = new int[4];
+        for (int i = 0; i < S; i++) {
+            if (strArr[i].equals("A")) intArr[0]++;
+            else if (strArr[i].equals("C")) intArr[1]++;
+            else if (strArr[i].equals("G")) intArr[2]++;
+            else if (strArr[i].equals("T")) intArr[3]++;
+        }
+
+        if (nums > P) {
+            System.out.println(answer);
+            return;
+        }
+        for (int i = 0; i < numArr.length; i++) {
+            if (intArr[i] < numArr[i]) {
+                System.out.println(answer);
+                return;
+            }
+        }
+
+        int s_i = 0;
+        int[] tmpArr = new int[4];
+        for (int s = s_i; s < s_i + P; s++) {
+            if (strArr[s].equals("A")) tmpArr[0]++;
+            else if (strArr[s].equals("C")) tmpArr[1]++;
+            else if (strArr[s].equals("G")) tmpArr[2]++;
+            else if (strArr[s].equals("T")) tmpArr[3]++;
+        }
+        if (tmpArr[0] >= numArr[0] && tmpArr[1] >= numArr[1] && tmpArr[2] >= numArr[2] && tmpArr[3] >= numArr[3]) {
+            answer++;
+        }
+        while (s_i < strArr.length - P) {
+            if (strArr[s_i].equals("A")) tmpArr[0]--;
+            else if (strArr[s_i].equals("C")) tmpArr[1]--;
+            else if (strArr[s_i].equals("G")) tmpArr[2]--;
+            else if (strArr[s_i].equals("T")) tmpArr[3]--;
+
+            int end = s_i+P;
+            if (strArr[end].equals("A")) tmpArr[0]++;
+            else if (strArr[end].equals("C")) tmpArr[1]++;
+            else if (strArr[end].equals("G")) tmpArr[2]++;
+            else if (strArr[end].equals("T")) tmpArr[3]++;
+
+            if (tmpArr[0] >= numArr[0] && tmpArr[1] >= numArr[1] && tmpArr[2] >= numArr[2] && tmpArr[3] >= numArr[3]) {
+                answer++;
+            }
+            s_i++;
+        }
+
+        System.out.println(answer);
+
+    }
+
+
+}
+
