6a9c2eb361f01dc9b5fd95ea2f5b086da91e96c2
ssooomm <101492513+ssooomm@users.noreply.github.com>
2024-06-28 16:31:57 +0900

    [level 1] Title: 예산, Time: 0.50 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] d, int budget) {
-        int answer = 0;
+        int answer = d.length;
         
         int d_sum = 0;
         Arrays.sort(d);
