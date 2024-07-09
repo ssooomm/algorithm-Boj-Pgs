a71e75e808b42fe82d78b1a5996f3db7f40cadb1
ssooomm <101492513+ssooomm@users.noreply.github.com>
2024-07-09 17:23:30 +0900

    [Gold V] Title: 동전 1, Time: 136 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N+1];
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] cnt = new int[k+1];
+        cnt[0] = 1;
+
+        for(int i=1;i<=N;i++){
+            for(int j = arr[0];j<=k;j++){
+                if(j<arr[i]){
+                    continue;
+                }
+                cnt[j] += cnt[j-arr[i]];
+            }
+        }
+
+        System.out.println(cnt[k]);
+    }
+}
