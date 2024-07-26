0005523fbb96921b85d011204cfcc6c9a6c4dd7c
ssooomm <101492513+ssooomm@users.noreply.github.com>
2024-07-26 10:21:49 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 1.55 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -4,13 +4,16 @@ class Solution {
         int answer = 0;
         Arrays.sort(numbers);
         int plus = numbers[numbers.length-1] * numbers[numbers.length-2];
-        int minus = 0;
-        int i=0;
-        for(;i<numbers.length;i++){
-            if(numbers[i]>0) break;
-        }
-        if(i>1) minus = numbers[i-1] * numbers[i-2];
-        else return plus;
+        
+        // int minus = 0;
+        // int i=0;
+        // for(;i<numbers.length;i++){
+        //     if(numbers[i]>0) break;
+        // }
+        // if(i>1) minus = numbers[i-1] * numbers[i-2];
+        // else return plus;
+        // 굳이 이럴 필요 없음, 걍 최하위 2개만 뽑기
+        int minus = numbers[0] * numbers[1];
         answer = (minus>plus)? minus:plus;
         return answer;
     }
