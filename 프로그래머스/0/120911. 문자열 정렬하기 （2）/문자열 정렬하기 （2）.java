352fadfb086d584aaef979f88cd436f1a1b0f30c
ssooomm <101492513+ssooomm@users.noreply.github.com>
2024-07-31 13:19:56 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.53 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -2,15 +2,19 @@ import java.util.*;
 
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
-        String[] arr = my_string.split("");
-        for(int i=0;i<arr.length;i++){
-            arr[i] = arr[i].toLowerCase();
-        }
-        Arrays.sort(arr);
-        for(String str:arr){
-            answer+=str;
-        }
-        return answer;
+        char[] c = my_string.toLowerCase().toCharArray();
+        Arrays.sort(c);
+        return new String(c);
+        
+        // String answer = "";
+        // String[] arr = my_string.split("");
+        // for(int i=0;i<arr.length;i++){
+        //     arr[i] = arr[i].toLowerCase();
+        // }
+        // Arrays.sort(arr);
+        // for(String str:arr){
+        //     answer+=str;
+        // }
+        // return answer;
     }
 }
