b761774f1a5cfe9c1eebc20f16e80c1882a97545
ssooomm <101492513+ssooomm@users.noreply.github.com>
2024-08-05 14:53:44 +0900

    [level 2] Title: 주식가격, Time: 25.48 ms, Memory: 69.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,16 +1,25 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] prices) {
-        // int[] answer = {};
         int[] answer = new int[prices.length];
-        for(int i=0; i<prices.length; i++) {
-            int v = prices[i];
-            for(int j=i+1; j<prices.length; j++) {
-                answer[i]++;
-                if(prices[j] < v) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        for(int i=0;i<prices.length;i++){
+            while(!stack.isEmpty()){
+                int j = stack.peek();
+                if(prices[j]>prices[i]){
+                    answer[j] = i-j;
+                    stack.pop();
+                }else{
                     break;
                 }
             }
-
+            stack.push(i);
+        }
+        
+        while(!stack.isEmpty()){
+            int i = stack.pop();
+            answer[i] = prices.length-i-1;
         }
         return answer;
     }
