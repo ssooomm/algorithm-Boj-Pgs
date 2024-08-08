eabc9a9d6cd0ef7674e5593de06357ef27f86046
ssooomm <101492513+ssooomm@users.noreply.github.com>
2024-08-08 23:23:46 +0900

    [level 3] Title: 네트워크, Time: 0.25 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,34 +1,25 @@
-
-import java.util.*;
-
-public class Solution {
-    static Map<Integer, List<Integer>> graph = new HashMap<>();
-    static Map<Integer, Boolean> visited = new HashMap<>();
-
+class Solution {
     public int solution(int n, int[][] computers) {
         int answer = 0;
+        
         boolean[] visited = new boolean[n];
-
+        
         for(int i=0;i<n;i++){
             if(!visited[i]){
-                dfs(i,n,computers,visited);
+                dfs(visited,computers,i,n);
                 answer++;
             }
         }
         return answer;
     }
-
-    void dfs(int cur, int n, int[][] computers, boolean[] visited) {
+    
+    void dfs(boolean[] visited, int[][] computers,int cur, int n){
         visited[cur] = true;
-
-        for (int i = 0; i < n; i++) {
-            if(computers[cur][i] == 1) {
-                if(!visited[i])
-                    dfs(i, n, computers, visited);
+        
+        for(int x=0;x<n;x++){
+            if(!visited[x] && computers[cur][x]==1){
+                dfs(visited,computers,x,n);
             }
         }
-
     }
-
 }
-
