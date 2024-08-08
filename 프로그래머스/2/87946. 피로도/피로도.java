92a5b1a8a3a4d374e2b821d6721af9604638e8dd
ssooomm <101492513+ssooomm@users.noreply.github.com>
2024-08-08 10:47:10 +0900

    [level 2] Title: 피로도, Time: 52.47 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,32 +1,47 @@
+import java.util.*;
 
 class Solution {
-    int answer = 0;
     public int solution(int k, int[][] dungeons) {
-        int n = dungeons.length;
-        boolean[] visited = new boolean[n];
-        backtrack(n,k,visited, 0,dungeons);
-        return answer;
-    }
-
-    public void backtrack(int n, int k, boolean[] visited, int cnt, int[][] dungeons) {
-        if(cnt > answer){
-            answer = cnt;
+        int answer = 0;
+        int len = dungeons.length;
+        List<List<Integer>> res = new ArrayList<>();
+        boolean[] visited = new boolean[len+1];
+        backtrack(len,visited,new ArrayList<>(), res);
+        
+        int max = 0;
+        for(List<Integer> l:res){
+            int tmp = k;
+            int cnt = 0;
+            for(int n: l){
+                if(tmp>=dungeons[n-1][0]){
+                    tmp-=dungeons[n-1][1];
+                    cnt++;
+                }
+                else{
+                    break;
+                }
+            }
+            if(max<cnt){
+                max = cnt;
+            }
         }
-        for(int i = 0; i < n; i++){
-            if(visited[i]) continue;
 
-            int least = dungeons[i][0];
-            int need = dungeons[i][1];
-
-            if(k>=least){
+        return max;
+    }
+    void backtrack(int n, boolean[] visited, List<Integer> cur, List<List<Integer>> res){
+        if(cur.size() == n){
+            res.add(new ArrayList<>(cur));
+            return;
+        }
+        
+        for(int i=1;i<=n;i++){
+            if(!visited[i]){
                 visited[i] = true;
-                k-=need;
-                backtrack(n,k,visited,cnt+1,dungeons);
-                k+=need;
+                cur.add(i);
+                backtrack(n,visited, cur, res);
+                cur.remove(cur.size()-1);
                 visited[i] = false;
             }
-
         }
     }
-
 }
