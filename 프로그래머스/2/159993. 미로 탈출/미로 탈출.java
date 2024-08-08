78a305462e43ad1264babffdb6c66392e44ebaa2
ssooomm <101492513+ssooomm@users.noreply.github.com>
2024-08-09 02:01:08 +0900

    [level 2] Title: 미로 탈출, Time: 5.87 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -1,69 +1,75 @@
 import java.util.*;
 
 class Solution {
-    //상하좌우
     int[] dr = {0,0,1,-1};
-    int[] dc = {1,-1,0,0}; 
-    int rowLen, colLen;
+    int[] dc = {1,-1,0,0};
+    
     public int solution(String[] maps) {
         int answer = 0;
-
-        rowLen = maps.length;
-        colLen = maps[0].length();
-        boolean[][] visited = new boolean[rowLen][colLen];
-        Queue<int[]> queue = new ArrayDeque<>();
-
-        int[] start = new int[2];
-        int[] lever= new int[2];
-        int[] exit= new int[2];
-
-
-        for(int r=0;r<rowLen;r++){
-            for(int c=0;c<colLen;c++){
+        int n = maps.length;
+        int m = maps[0].length();
+        
+        Queue<int[]> q = new ArrayDeque<>();
+        boolean[][] visited = new boolean[n][m];
+        
+        for(int r=0;r<n;r++){
+            for(int c=0;c<m;c++){
                 if(maps[r].charAt(c)=='S'){
-                    start = new int[]{r,c};
-                }else if(maps[r].charAt(c)=='L'){
-                    lever = new int[]{r,c};
-                }else if(maps[r].charAt(c)=='E'){
-                    exit = new int[]{r,c};
+                    q.add(new int[]{r,c,0});
+                    visited[r][c] = true;
                 }
             }
         }
-
-        int toLever = bfs(start,lever,maps);
-        if(toLever==-1){ return - 1; }
         
-        int toExit = bfs(lever, exit, maps);
-        if(toExit==-1){ return -1; }
-        
-        return toLever + toExit;
-    }
-    
-    public int bfs(int[] start, int[] target, String[] maps){
-        boolean[][] visited = new boolean[rowLen][colLen];
-        Queue<int[]> queue = new ArrayDeque<>();
-        queue.offer(new int[]{start[0],start[1],0});
-        visited[start[0]][start[1]] = true;
-        
-        while(!queue.isEmpty()){
-            int[] cur = queue.poll();
+        int toL = 0, toLR=0, toLC=0;
+        while(!q.isEmpty()){
+            int[] cur = q.remove();
             int r = cur[0];
-            int c = cur[1];
+            int c= cur[1];
             int d = cur[2];
-            if(r==target[0]&&c==target[1]){ return d; }
-            
-            
+            if(maps[r].charAt(c)=='L'){
+                toL += d;
+                toLR = r;
+                toLC = c;
+                break;
+            }
             for(int i=0;i<4;i++){
                 int nr = r+dr[i];
                 int nc = c+dc[i];
-
-                if(nr>=0&&nc>=0&&nr<rowLen&&nc<colLen&&maps[nr].charAt(nc) != 'X'&&!visited[nr][nc]){
-                    //해당 타겟까지의 거리까지 포함한 1을 더해서 return 
-                    queue.offer(new int[]{nr,nc,d+1});
+                if(nr>=0&&nr<n&&nc>=0&&nc<m&&!visited[nr][nc]&&maps[nr].charAt(nc)!='X'){
                     visited[nr][nc] = true;
+                    q.add(new int[]{nr,nc,d+1});
                 }
             }
         }
-        return -1;
+        if(toL==0) return -1;
+        
+        q = new ArrayDeque<>();
+        visited = new boolean[n][m];
+        q.add(new int[]{toLR,toLC,0});
+        visited[toLR][toLC] = true;
+        
+        int toE=0;
+        while(!q.isEmpty()){
+            int[] cur = q.remove();
+            int r = cur[0];
+            int c= cur[1];
+            int d = cur[2];
+            if(maps[r].charAt(c)=='E'){
+                toE += d;
+                break;
+            }
+            for(int i=0;i<4;i++){
+                int nr = r+dr[i];
+                int nc = c+dc[i];
+                if(nr>=0&&nr<n&&nc>=0&&nc<m&&!visited[nr][nc]&&maps[nr].charAt(nc)!='X'){
+                    visited[nr][nc] = true;
+                    q.add(new int[]{nr,nc,d+1});
+                }
+            }
+        }
+        if(toE==0) return -1;
+        
+        return toL+toE;
     }
 }
