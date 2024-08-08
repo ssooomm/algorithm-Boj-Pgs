b747448f3167df1e821bf89c8f48565057dd1988
ssooomm <101492513+ssooomm@users.noreply.github.com>
2024-08-09 01:02:28 +0900

    [level 3] Title: 단어 변환, Time: 0.59 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,49 +1,40 @@
 import java.util.*;
+
 class Solution {
     public int solution(String begin, String target, String[] words) {
-        Queue<WordState> queue = new ArrayDeque<>(); //큐에 저장할 거 클래스로 만들수 있음
+        int answer = 0;
+        Queue<WordState> q = new ArrayDeque<>();
         boolean[] visited = new boolean[words.length];
-
-        queue.offer(new WordState(begin,0));
-
-        while (!queue.isEmpty()) {
-            // 방문
-            WordState curr = queue.poll();
-
-            // 방문한 곳이 내가 찾던 타겟이라면 return cnt
-            if(curr.word.equals(target)) {
-                return curr.cnt;
-            }
-
-            // nextVertex 예약
-            // 원래는 nextVertex만 예약하면 됨, but 이 문제는 알 수 없음,
-            // 내가 찾아야함
-            for(int i = 0; i < words.length; i++) {
-                if(!visited[i] && getDiffCnt(curr.word,words[i])==1) {
+        q.add(new WordState(0,begin));
+        
+        while(!q.isEmpty()){
+            WordState cur = q.remove();
+            if(cur.word.equals(target)) return cur.cnt;
+            for(int i=0;i<words.length;i++){
+                if(!visited[i]&&getDiff(cur.word,words[i])==1){
                     visited[i] = true;
-                    queue.offer(new WordState(words[i],curr.cnt + 1));
+                    q.add(new WordState(cur.cnt+1,words[i]));
                 }
             }
         }
-        return 0;
+        return answer;
     }
-
-    // 큐에 저장할 변수를 클래스로 만들어줌
+    int getDiff(String cur, String target){
+        int cnt = 0;
+        // for(String c:cur.split("")){
+        //     if(!target.contains(c)) cnt++;
+        // }
+        for(int i=0;i<cur.length();i++){
+            if(cur.charAt(i) != target.charAt(i)) cnt++;
+        }
+        return cnt;
+    }
     class WordState{
+        int cnt;
         String word;
-        int cnt; //몇 번 바뀌나
-        WordState(String word, int cnt){
-            this.word = word;
+        WordState(int cnt,String word){
             this.cnt = cnt;
+            this.word = word;
         }
-    }
-
-    // curr.word vs words[i]
-    int getDiffCnt(String word, String target){
-        int diffCnt = 0;
-        for(int i = 0; i < word.length(); i++){
-            if(word.charAt(i) != target.charAt(i)){diffCnt++;}
-        }
-        return diffCnt;
     }
 }
