662fb8d25e696df0f86284715fdde9c6deee6468
ssooomm <101492513+ssooomm@users.noreply.github.com>
2024-08-09 12:37:12 +0900

    [level 3] Title: 가장 먼 노드, Time: 33.59 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,6 +1,39 @@
+import java.util.*;
+
 class Solution {
     public int solution(int n, int[][] edge) {
         int answer = 0;
-        return answer;
+        Map<Integer,List<Integer>> graph = new HashMap<>();
+        for(int i=1;i<=n;i++){
+            graph.put(i,new ArrayList<>());
+        }
+        for(int[] e:edge){
+            graph.get(e[0]).add(e[1]);
+            graph.get(e[1]).add(e[0]);
+        }
+        
+        Queue<int[]> q = new ArrayDeque<>();
+        boolean[] visited = new boolean[n+1];
+        q.add(new int[]{1,0});
+        visited[1] = true;
+        
+        int max=0,cnt=0;
+        while(!q.isEmpty()){
+            int[] cur = q.remove();
+            if(cur[1]>max){
+                max= cur[1];
+                cnt = 1;
+            }else if(cur[1]==max) cnt++;
+            
+            for(int next:graph.get(cur[0])){
+                if(!visited[next]){
+                    visited[next] = true;
+                    q.add(new int[]{next,cur[1]+1});
+                }
+            }
+        }
+        
+        
+        return cnt;
     }
 }
