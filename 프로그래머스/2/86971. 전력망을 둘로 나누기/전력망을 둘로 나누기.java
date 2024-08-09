7b09c4a461cdab51f834157eddf5e6f4b709ec40
ssooomm <101492513+ssooomm@users.noreply.github.com>
2024-08-09 13:03:03 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 6.02 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -12,30 +12,39 @@ class Solution {
             graph.get(w[1]).add(w[0]);
         }
         
-        for(int[] i:wires){
-            int a = bfs(i[0],n,graph,i[1]);
-            int b = bfs(i[1],n,graph,i[0]);
-            answer = Math.min(answer,Math.abs(a-b));
+        for(int[] w:wires){
+            // boolean[] v1= new boolean[n+1];
+            // v1[w[1]] = true;
+            // int n1 = bfs(w[0],w[1],n,graph,v1);
+            boolean[] v1= new boolean[n+1];
+            v1[w[1]] = true;
+            int n1 = bfs(v1, graph,w[0]);
+            
+            // boolean[] v2= new boolean[n+1];
+            // v2[w[0]] = true;
+            // int n2 = bfs(v2,graph,w[1]);
+            
+            answer = Math.min(answer,Math.abs(n-2*n1));
         }
         return answer;
     }
     
-    int bfs(int e, int n, Map<Integer,List<Integer>> graph, int pass){
+    int bfs(boolean[] visited,Map<Integer,List<Integer>> graph, int st){
         Queue<Integer> q = new ArrayDeque<>();
-        boolean[] visited = new boolean[n+1];
-        q.add(e);
-        visited[e] = true;
-        int cnt=0;
+        q.add(st);
+        visited[st] = true;
+        int cnt = 1;
         while(!q.isEmpty()){
-            int cur = q.remove();
-            for(int ne:graph.get(cur)){
-                if(!visited[ne] && ne!=pass){
-                    visited[ne] =true;
-                    q.add(ne);
+            int target = q.remove();
+            for(int next:graph.get(target)){
+                if(!visited[next]){
+                    visited[next] = true;
+                    q.add(next);
                     cnt++;
                 }
             }
         }
+        
         return cnt;
     }
 }
