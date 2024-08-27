0b9fb1938262a2ed085008e460df9f9b233eda5b
ssooomm <101492513+ssooomm@users.noreply.github.com>
2024-08-27 10:03:01 +0900

    [level 2] Title: 소수 찾기, Time: 6.66 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,48 +1,52 @@
 import java.util.*;
 
 class Solution {
-    Set<Integer> candi = new HashSet<>();
+    Set<Integer> arr = new HashSet<>();
+    
     public int solution(String numbers) {
         int answer = 0;
         
-        permutation(numbers,new boolean[numbers.length()],0,0);
+        permutation(numbers, new boolean[numbers.length()], 0,0);
         
-        for(int c:candi){
-            if(isPrime(c)) answer++;
+        for(int s:arr){
+            if(isPrime(s)) answer++;
         }
         return answer;
     }
     
-    void permutation(String numbers,boolean[] visited, int cur, int digit){
-        if(digit==numbers.length()) return;
+    void permutation(String numbers, boolean[] visited, int cur, int digit){
+        if(digit==numbers.length()){
+            return;
+        }
+        
         for(int i=0;i<numbers.length();i++){
             if(!visited[i]){
-                visited[i]= true;
-                // int newV = cur+(int)((numbers.charAt(i)-'0')*Math.pow(10,digit));
-                int newV = cur + (int)((numbers.charAt(i) - '0') * Math.pow(10, digit));
-                candi.add(newV);
-                permutation(numbers,visited,newV,digit+1);
+                visited[i] = true;
+                int next = cur+(int)((numbers.charAt(i)-'0')*Math.pow(10,digit));
+                arr.add(next);
+                permutation(numbers, visited, next,digit+1);
                 visited[i] = false;
             }
         }
     }
     
-    // boolean isPrime(int n){
-    //     if(n<2) return false;
-    //     if(n%2==0 || n%3==0) return false;
-    //     int sq = (int)Math.sqrt(n);
+    // boolean isPrime(int num){
+    //     if(num<2) return false;
+    //     if(num==2 || num==3) return true;
+    //     int sq = (int)Math.sqrt(num);
     //     for(int i=4;i<=sq;i++){
-    //         if(n%i==0) return false;
+    //         if(num%i==0) return false;
     //     }
     //     return true;
     // }
-    boolean isPrime(int n) {
-        if (n < 2) return false;
-        
-        for (int i = 2; i * i <= n; i++) {
-            if (n % i == 0) return false;
+    public boolean isPrime(int n){
+        if(n<=1) return false;
+        if(n==2) return true;
+        if(n%2==0) return false; //짝수 걸러짐
+        int sqrt = (int) Math.sqrt(n);
+        for(int i=3;i<=sqrt;i+=2){ //짝수 제외, 홀수만 판별
+            if(n%i==0) return false;
         }
-        
         return true;
     }
 }
