a907558dd09fb7fff3814007d9477a8594209d61
ssooomm <101492513+ssooomm@users.noreply.github.com>
2024-08-29 14:37:10 +0900

    [level 3] Title: 네트워크, Time: 0.25 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,22 +1,24 @@
 class Solution {
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        
         boolean[] visited = new boolean[n];
-        for(int row=0;row<n;row++){
-            if(!visited[row]){
-                dfs(visited,computers,row,n);
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                dfs(computers,visited,i,n);
                 answer++;
             }
         }
+        
         return answer;
     }
     
-    void dfs(boolean[] visited, int[][] computers, int cur, int n){
+    void dfs(int[][] computers, boolean[] visited,int cur, int n){
         visited[cur] = true;
         for(int col=0;col<n;col++){
-            if(!visited[col]&&computers[cur][col]==1){
-                dfs(visited,computers,col,n);
+            if(!visited[col]){
+                if(computers[cur][col]==1){
+                    dfs(computers,visited,col,n);
+                }
             }
         }
     }
