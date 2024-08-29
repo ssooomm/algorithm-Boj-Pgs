c22a74109e7aba8b2f1b12f65b21cea58e8dda25
ssooomm <101492513+ssooomm@users.noreply.github.com>
2024-08-30 00:02:53 +0900

    [level 3] Title: 합승 택시 요금, Time: 33.52 ms, Memory: 66.1 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -6,64 +6,55 @@ class Edge implements Comparable<Edge>{
         this.node = node;
         this.cost = cost;
     }
-    
     @Override
     public int compareTo(Edge other){
         return Integer.compare(this.cost, other.cost);
     }
 }
-
 class Solution {
+    int INF = Integer.MAX_VALUE;   
     public int solution(int n, int s, int a, int b, int[][] fares) {
+        int answer = 0;
         Map<Integer,List<Edge>> graph = new HashMap<>();
         for(int i=1;i<=n;i++){
             graph.put(i,new ArrayList<>());
         }
         for(int[] fare:fares){
-            int n1 = fare[0];
-            int n2 = fare[1];
-            int w = fare[2];
-            graph.get(n1).add(new Edge(n2,w));
-            graph.get(n2).add(new Edge(n1,w));
+            graph.get(fare[0]).add(new Edge(fare[1],fare[2]));
+            graph.get(fare[1]).add(new Edge(fare[0],fare[2]));
         }
         
-        int[] sDist = dijkstra(graph, s, n);
-        int[] aDist = dijkstra(graph, a, n);
-        int[] bDist = dijkstra(graph, b, n);
+        int[] sD = dikstra(fares,graph,s,n);
+        int[] aD = dikstra(fares,graph,a,n);
+        int[] bD = dikstra(fares,graph,b,n);
         
-        int minC = Integer.MAX_VALUE;
-        
+        int minTime = INF;
         for(int i=1;i<=n;i++){
-            int cost = sDist[i]+aDist[i]+bDist[i];
-            minC = Math.min(minC, cost);
+            int tmp = sD[i]+aD[i]+bD[i];
+            minTime = Math.min(tmp,minTime);
         }
-        
-        return minC;
+        return minTime;
     }
     
-    int[] dijkstra(Map<Integer,List<Edge>> graph, int s, int n){
-        int INF = Integer.MAX_VALUE;
+    public int[] dikstra(int[][] fares, Map<Integer,List<Edge>> graph, int st, int n){
+        Queue<Edge> pq = new PriorityQueue<>();
+        pq.add(new Edge(st,0));
         int[] costs = new int[n+1];
         Arrays.fill(costs,INF);
-        
-        Queue<Edge> pq = new PriorityQueue<>();
-        pq.add(new Edge(s,0));
-        costs[s] = 0;
+        costs[st] = 0;
         
         while(!pq.isEmpty()){
             Edge cur = pq.remove();
-            if(costs[cur.node]<cur.cost) continue;
-            
+            if(cur.cost>costs[cur.node]) continue;
             for(Edge next: graph.get(cur.node)){
-                int nextC = costs[cur.node]+next.cost;
-                if(nextC < costs[next.node]){
-                    pq.add(new Edge(next.node, nextC));
-                    costs[next.node] = nextC;
+                int nextCost = costs[cur.node] + next.cost;
+                if(nextCost<costs[next.node]){
+                    costs[next.node] = nextCost;
+                    pq.add(new Edge(next.node,nextCost));
                 }
             }
         }
         
         return costs;
-        
     }
 }
