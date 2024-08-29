2ed343287bb7368e017a465e1daaab7b7d4d6d70
ssooomm <101492513+ssooomm@users.noreply.github.com>
2024-08-29 15:28:25 +0900

    [level 3] Title: 단어 변환, Time: 0.52 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,17 +1,25 @@
 import java.util.*;
 
+class WordState{
+    int cnt;
+    String word;
+    public WordState(int cnt, String word){
+        this.cnt = cnt;
+        this.word = word;
+    }
+}
 class Solution {
     public int solution(String begin, String target, String[] words) {
         int answer = 0;
         Queue<WordState> q = new ArrayDeque<>();
-        boolean[] visited = new boolean[words.length];
         q.add(new WordState(0,begin));
+        boolean[] visited = new boolean[words.length];
         
         while(!q.isEmpty()){
             WordState cur = q.remove();
             if(cur.word.equals(target)) return cur.cnt;
             for(int i=0;i<words.length;i++){
-                if(!visited[i]&&getDiff(cur.word,words[i])==1){
+                if(!visited[i] && getDiff(cur.word, words[i])==1){
                     visited[i] = true;
                     q.add(new WordState(cur.cnt+1,words[i]));
                 }
@@ -19,22 +27,12 @@ class Solution {
         }
         return answer;
     }
+    
     int getDiff(String cur, String target){
-        int cnt = 0;
-        // for(String c:cur.split("")){
-        //     if(!target.contains(c)) cnt++;
-        // }
+        int answer = 0;
         for(int i=0;i<cur.length();i++){
-            if(cur.charAt(i) != target.charAt(i)) cnt++;
+            if(cur.charAt(i)!=target.charAt(i)) answer++;
         }
-        return cnt;
-    }
-    class WordState{
-        int cnt;
-        String word;
-        WordState(int cnt,String word){
-            this.cnt = cnt;
-            this.word = word;
-        }
+        return answer;
     }
 }
