af259c99a700e55a4146a2662441fe4980df02d9
ssooomm <101492513+ssooomm@users.noreply.github.com>
2024-08-29 16:24:23 +0900

    [level 2] Title: N-Queen, Time: 262.93 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -1,27 +1,23 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n) {
-        boolean[][] board= new boolean[n][n];
+        boolean[][] board = new boolean[n][n];
         return place(board,0,n);
     }
-    int place(boolean[][] board,int r, int n){
-        if(r==n){
-            return 1;
-        }
+    int place(boolean[][] board, int r, int n){
+        if(r==n) return 1;
         int cnt = 0;
         
-        for(int c=0;c<n;c++){
-            if(!board[r][c] && check(board,r,c,n)){
-                board[r][c] = true;
-                cnt+=place(board,r+1,n);
-                board[r][c] = false;
+        for(int col=0;col<n;col++){
+            if(!board[r][col] && check(board,r,col,n)){
+                board[r][col] = true;
+                cnt += place(board,r+1,n);
+                board[r][col] = false;
             }
         }
         return cnt;
     }
     
-    boolean check(boolean[][] board, int r, int c,int n){
+    boolean check(boolean[][] board, int r, int c, int n){
         for(int i=0;i<r;i++){
             if(board[i][c]) return false;
         }
@@ -29,7 +25,6 @@ class Solution {
         for(int i=r-1,j=c-1;i>=0&&j>=0;i--,j--){
             if(board[i][j]) return false;
         }
-        
         for(int i=r-1,j=c+1;i>=0&&j<n;i--,j++){
             if(board[i][j]) return false;
         }
