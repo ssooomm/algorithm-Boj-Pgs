0961cd72d8f36c28c186ebbf5b93221151885b10
ssooomm <101492513+ssooomm@users.noreply.github.com>
2024-10-01 22:17:59 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.03 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        int n = gcd(a,b);
+        b/=n;
+        while(b%2==0){
+            b/=2;
+        }
+        while(b%5==0){
+            b/=5;
+        }
+        return b==1?1:2;
+    }
+    
+    int gcd(int a, int b){
+        if(b==0) return a;
+        return gcd(b,a%b);
+    }
+}
