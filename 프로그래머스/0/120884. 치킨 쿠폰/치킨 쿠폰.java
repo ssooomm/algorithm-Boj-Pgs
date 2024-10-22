896e969a6064f92aeea85a4cf5813bee29a36202
ssooomm <101492513+ssooomm@users.noreply.github.com>
2024-10-22 09:53:17 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.03 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -1,11 +1,13 @@
 class Solution {
     public int solution(int chicken) {
         int answer = 0;
-        while(chicken>=10){
-            int tmp = chicken;
-            chicken/=10;
-            answer += chicken;
-            chicken += tmp%10;
+        int coupon = chicken;
+        int ch =chicken;
+        while(ch>0){
+            ch = coupon/10;
+            answer+=ch;
+            coupon %=10;
+            coupon+=ch;
         }
         return answer;
     }
