e99ab0de6ef031eb0989a50c55b4761b7380e48d
ssooomm <101492513+ssooomm@users.noreply.github.com>
2024-10-29 21:04:28 +0900

    [level 2] Title: 타겟 넘버, Time: 8.69 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,17 +1,24 @@
 class Solution {
+    static int count;
+    static int N;
     public int solution(int[] numbers, int target) {
-        return backtrack(0,0,numbers,target);
+        count = 0;
+        N = numbers.length;
+        
+        dfs(0,0,numbers,target);
+        return count;
+    }
+    public void dfs(int depth, int tot, int[] numbers, int target){
+        if(depth==N){
+            //로직
+            if(tot==target){
+                count++;
+            }
+            return;
+        }
+        dfs(depth+1,tot+numbers[depth],numbers,target);
+        dfs(depth+1,tot-numbers[depth],numbers,target);
+        
     }
     
-    public int backtrack(int cur, int idx, int[] numbers, int target){
-        if(idx==numbers.length){
-            if(cur==target) return 1;
-            else return 0;
-        }
-        
-        int sum = 0;
-        sum += backtrack(cur+numbers[idx],idx+1,numbers,target);
-        sum += backtrack(cur-numbers[idx],idx+1,numbers,target);
-        return sum;
-    }
 }
