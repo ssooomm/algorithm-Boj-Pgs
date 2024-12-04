d48d5e3b876d3e99ab7a4ed926438a107f160c69
ssooomm <101492513+ssooomm@users.noreply.github.com>
2024-12-04 20:35:48 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 83.12 ms, Memory: 97.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,18 +3,16 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-                HashMap<String,Integer> hm = new HashMap<>();
-
-//        getOrDefault - 찾는 키가 존재한다면 찾는 키의 값을 반환하고 없다면 기본 값을 반환하는 메서드
-        for(String p:participant){
-            hm.put(p,hm.getOrDefault(p,0)+1);
+        Map<String, Integer> m = new HashMap<>();
+        for(String p: participant){
+            m.put(p,m.getOrDefault(p,0)+1);
         }
         for(String c:completion){
-            hm.put(c,hm.get(c)-1);
+            m.put(c,m.get(c)-1);
         }
-        for(String key:hm.keySet()){
-            if(hm.get(key)!=0){
-                answer += key;
+        for(String str:m.keySet()){
+            if(m.get(str)!=0){
+                return str;
             }
         }
         return answer;
