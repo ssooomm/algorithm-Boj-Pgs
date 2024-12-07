0d426076e564793e7bb54c242cc5729dd5530648
ssooomm <101492513+ssooomm@users.noreply.github.com>
2024-12-07 17:31:34 +0900

    [level 2] Title: 타겟 넘버, Time: 5.23 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,25 +1,15 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] numbers, int target) {
-        
-        
-        
-        
         return dfs(0,0,numbers,target);
     }
-    
     int dfs(int curr, int idx, int[] numbers, int target){
         if(idx==numbers.length){
-            if(curr==target){
-                return 1;
-            }
-            return 0;
+            if(curr==target) return 1;
+            else return 0;
         }
-        
         int cnt=0;
-        cnt += dfs(curr+numbers[idx], idx+1, numbers,target);
-        cnt += dfs(curr-numbers[idx], idx+1, numbers,target);
+        cnt+=dfs(curr+numbers[idx], idx+1, numbers, target);
+        cnt+=dfs(curr-numbers[idx], idx+1, numbers, target);
         return cnt;
     }
 }
