c53549140de56c2ea85d6535d6e5e18774009f84
ssooomm <101492513+ssooomm@users.noreply.github.com>
2024-12-08 12:21:44 +0900

    [level 3] Title: 단어 변환, Time: 0.71 ms, Memory: 66.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,39 +1,41 @@
 import java.util.*;
 
-class WordState{
+class WS{
     int cnt;
     String word;
-    WordState(int cnt, String word){
-        this.cnt = cnt;
+    WS(int cnt, String word){
+        this.cnt=cnt;
         this.word = word;
     }
 }
 class Solution {
+    
     public int solution(String begin, String target, String[] words) {
         int answer = 0;
-        
-        Queue<WordState> q = new ArrayDeque<>();
-        q.add(new WordState(0,begin));
-        
+        Queue<WS> q = new ArrayDeque<>();
+        q.add(new WS(0,begin));
         boolean[] visited = new boolean[words.length];
         
         while(!q.isEmpty()){
-            WordState curr = q.remove();
+            WS curr = q.remove();
             if(curr.word.equals(target)) return curr.cnt;
             for(int i=0;i<words.length;i++){
-                if(!visited[i] && getDiff(curr.word,words[i])==1){
-                    q.add(new WordState(curr.cnt+1, words[i]));
+                if(!visited[i] && getDiff(curr.word, words[i])==1){
+                    q.add(new WS(curr.cnt+1, words[i]));
                     visited[i] = true;
                 }
             }
         }
+        
+        
         return answer;
     }
     
-    public int getDiff(String target, String cmp){
-        int cnt = 0;
-        for(int i=0;i<target.length();i++){
-            if(target.charAt(i) != cmp.charAt(i)) cnt++;
+    
+    public int getDiff(String a, String b){
+        int cnt=0;
+        for(int i=0;i<a.length();i++){
+            if(a.charAt(i)!=b.charAt(i)) cnt++;
         }
         return cnt;
     }
