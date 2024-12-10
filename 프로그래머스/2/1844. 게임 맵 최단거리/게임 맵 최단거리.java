8235f354058fdaf6da4d31b0d25ff90c3a9fa5ee
ssooomm <101492513+ssooomm@users.noreply.github.com>
2024-12-10 14:08:57 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.38 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,29 +1,31 @@
 import java.util.*;
 
 class Solution {
+    int[] dr = {0,0,1,-1};
+    int[] dc = {1,-1,0,0};
+    
     public int solution(int[][] maps) {
         int answer = -1;
         int n = maps.length;
         int m = maps[0].length;
-        Queue<int[]> q = new ArrayDeque<>();
         boolean[][] visited = new boolean[n][m];
+        
+        Queue<int[]> q = new ArrayDeque<>();
         q.add(new int[]{0,0,1});
         visited[0][0] = true;
         
-        int[] dr = {0,0,1,-1};
-        int[] dc = {1,-1,0,0};
         while(!q.isEmpty()){
-            int[] cur = q.remove();
-            int r= cur[0];
-            int c = cur[1];
-            int d = cur[2];
-            if(r==n-1&&c==m-1) return d;
+            int[] curr = q.remove();
+            if(curr[0]==n-1 && curr[1]==m-1) return curr[2];
             for(int i=0;i<4;i++){
-                int nr = r+dr[i];
-                int nc = c+dc[i];
-                if(nr>=0&&nr<n&&nc>=0&&nc<m&&!visited[nr][nc]&&maps[nr][nc]==1){
-                    visited[nr][nc] = true;
-                    q.add(new int[]{nr, nc, d+1});
+                int nr = curr[0] + dr[i];
+                int nc = curr[1] + dc[i];
+                
+                if(nr>=0 && nr<n && nc>=0 && nc<m){
+                    if(maps[nr][nc]==1 && !visited[nr][nc]){
+                        q.add(new int[]{nr,nc, curr[2]+1});
+                        visited[nr][nc] = true;
+                    }
                 }
             }
         }
