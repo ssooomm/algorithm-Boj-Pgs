b9a363b07751cdb028e5c6d32a26b3d3d5e48281
ssooomm <101492513+ssooomm@users.noreply.github.com>
2024-12-12 12:30:35 +0900

    [level 3] Title: 가장 먼 노드, Time: 55.79 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -3,38 +3,40 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[][] edge) {
         int answer = 0;
-        Map<Integer, List<Integer>> graph = new HashMap<>();
+        boolean[] visited = new boolean[n+1];
+        Map<Integer,List<Integer>> graph = new HashMap<>();
         for(int i=1;i<=n;i++){
-            graph.put(i,new ArrayList<>());
+            graph.put(i, new ArrayList<>());
         }
-        for(int[] e:edge){
+        for(int[] e: edge){
             graph.get(e[0]).add(e[1]);
             graph.get(e[1]).add(e[0]);
         }
         
-        boolean[] visited = new boolean[n+1];
+        visited[1] = true;
         Queue<int[]> q = new ArrayDeque<>();
         q.add(new int[]{1,0});
-        visited[1] = true;
-        int maxDist=0,cnt=0;
-        
+        int maxD = 0, cnt = 0;
         while(!q.isEmpty()){
-            int[] cur = q.remove();
-            if(maxDist<cur[1]){
-                maxDist = cur[1];
-                cnt = 1;
-            }else if(maxDist==cur[1]){
+            int[] curr = q.remove();
+            if(maxD<curr[1]){
+                maxD = curr[1];
+                cnt= 1;
+            }else if(maxD==curr[1]){
                 cnt++;
             }
-            
-            for(int g:graph.get(cur[0])){
+            for(int g:graph.get(curr[0])){
                 if(!visited[g]){
                     visited[g] = true;
-                    q.add(new int[]{g,cur[1]+1});
+                    q.add(new int[]{g, curr[1]+1});
                 }
             }
             
         }
+        
+        
+        
+        
         return cnt;
     }
 }
