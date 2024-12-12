9b3c8d3b4a2acf0e7c1f847bbc0775cdd84c3d0d
ssooomm <101492513+ssooomm@users.noreply.github.com>
2024-12-12 20:41:02 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 11.78 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -3,40 +3,39 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[][] wires) {
         int answer = Integer.MAX_VALUE;
-        Map<Integer,List<Integer>> graph = new HashMap<>();
-        for(int i=1;i<=n;i++){
-            graph.put(i,new ArrayList<>());
-        }
-        for(int[] w:wires){
-            graph.get(w[0]).add(w[1]);
-            graph.get(w[1]).add(w[0]);
-        }
         
-        for(int[] w:wires){
-            int a = bfs(graph, n, w[0], w[1]);
-            answer = Math.min(answer, Math.abs(n-2*a));
+        Map<Integer,List<Integer>> graph = new HashMap<>();
+        
+        
+        for(int i=0;i<n-1;i++){
+            for(int a=1;a<=n;a++){
+                graph.put(a,new ArrayList<>());
+            }
+            for(int j=0;j<n-1;j++){
+                if(i==j) continue;
+                graph.get(wires[j][0]).add(wires[j][1]);
+                graph.get(wires[j][1]).add(wires[j][0]);
+            }
+            // System.out.println(graph);
+            // boolean[] visited = new boolean[n+1];
+            Set<Integer> visited = new HashSet<>();
+            dfs(graph, wires[i][0], visited);
+            int x = visited.size();
+            visited.clear();
+            dfs(graph, wires[i][1], visited);
+            int y = visited.size();
+            int tmp = Math.abs(x-y);
+            answer = Math.min(tmp, answer);
         }
         return answer;
     }
     
-    int bfs(Map<Integer,List<Integer>> graph, int n, int e, int pass){
-        boolean[] visited= new boolean[n+1];
-        visited[e] = true;
-        visited[pass] = true;
-        Queue<Integer> q = new ArrayDeque<>();
-        q.add(e);
-        
-        int cnt = 1;
-        while(!q.isEmpty()){
-            int cur = q.remove();
-            for(int g:graph.get(cur)){
-                if(!visited[g]){
-                    visited[g] = true;
-                    q.add(g);
-                    cnt++;
-                }
+    void dfs(Map<Integer,List<Integer>> graph, int curr, Set<Integer> visited){
+        visited.add(curr);
+        for(Integer g:graph.get(curr)){
+            if(!visited.contains(g)){
+                dfs(graph, g, visited);
             }
         }
-        return cnt;
     }
 }
