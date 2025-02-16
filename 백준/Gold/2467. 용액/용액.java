1358e2d42a79015e7517918dd7bcda3df7e002b5
ssooomm <101492513+ssooomm@users.noreply.github.com>
2025-02-16 20:32:09 +0900

    [Gold V] Title: 용액, Time: 348 ms, Memory: 28612 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -1,37 +1,38 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		
-		long[] arr = new long[n];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i=0; i<n; i++) {
-			arr[i] = Long.parseLong(st.nextToken());
-		}
-		
-		int left =0;
-		int right =n-1;
-		int ml =0, mr = 0;
-		long min = Long.MAX_VALUE;
-		while(left<right) {
-			long sum = arr[left]+arr[right];
-			if(min > Math.abs(sum)) {
-				min = Math.abs(sum);
-				ml = left; mr = right;
-			}
-			if(sum>=0) {
-				right--;	
-			}else {
-				left++;
-			}
-		}
-		System.out.println(arr[ml] +" "+arr[mr]);
-	}
+public class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        int min = Integer.MAX_VALUE;
+        int ml=0,mr=0;
+        for(int i=0;i<n-1;i++){
+            int l = i+1;
+            int r = n-1;
+            while(l<=r){
+                int mid = (l+r)/2;
+                int sum = Math.abs(arr[i]+arr[mid]);
+                
+                if(min>sum){
+                    min = sum;
+                    ml = i;
+                    mr = mid;
+                }
+                if(arr[mid]>=-arr[i]){
+                    r = mid-1;
+                }else{
+                    l = mid+1;
+                }
+            }
+        }
+        System.out.println(arr[ml]+" "+arr[mr]);
+    }
 }
