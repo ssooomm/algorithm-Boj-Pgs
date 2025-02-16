34b96d0c7b9593c61defb5a85748331dd26f3027
ssooomm <101492513+ssooomm@users.noreply.github.com>
2025-02-16 17:54:46 +0900

    [Silver I] Title: 기타 레슨, Time: 292 ms, Memory: 23912 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.java
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.java
@@ -1,51 +1,44 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class Main{
+    public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        int[] lessonList = new int[n];
-
-        int left = 0;
-        int right = 0;
+        
+        int[] arr = new int[n];
+        int l=0;
+        int r=0;
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            lessonList[i] = Integer.parseInt(st.nextToken());
-            right += lessonList[i];
-            left = Math.max(left, lessonList[i]);
+        for(int i=0;i<n;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+            r+=arr[i]; //총 합
+            l = Math.max(l,arr[i]); //제일 큰 값
         }
-
-        while (left <= right) {
-            int mid = (left + right) / 2;
-
-            int count = getCount(n, lessonList, mid);
-
-            if(count > m){
-                left = mid + 1;
-            }else{
-                right = mid - 1;
-            }
+        
+        while(l<=r){
+            int mid = (l+r)/2;
+            int cnt = getCnt(n,arr,mid);
+            
+            if(cnt>m) l = mid+1; //m개 보다 적음
+            else r = mid-1;
         }
-
-        System.out.println(left);
+        System.out.println(l);
     }
-
-    private static int getCount(int n, int[] lessonList, int mid) {
-        int sum = 0;
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (sum + lessonList[i] > mid) {
-                sum = 0;
-                count++;
+    
+    static int getCnt(int n, int[] arr, int mid){
+        int sum=0;
+        int cnt = 0;
+        for(int i=0;i<n;i++){
+            if(sum+arr[i]>mid){ //중간값 보다 누적합이 커지면
+                sum=0;
+                cnt++;
             }
-            sum += lessonList[i];
+            sum+=arr[i];
         }
-
-        if(sum != 0) count++;
-        return count;
+        if(sum!=0) cnt++;
+        return cnt;
     }
 }
