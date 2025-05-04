69702639f84af4c71ff3150395f8e529d60d9e76
ssooomm <101492513+ssooomm@users.noreply.github.com>
2025-05-04 18:42:57 +0900

    [Silver III] Title: 퇴사, Time: 100 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,33 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class Main{
+    public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-
-        int[] T = new int[N+1];
-        int[] P = new int[N+1];
-        int[] dp = new int[N+2];
+        int n = Integer.parseInt(br.readLine());
         StringTokenizer st;
-        for(int i = 1; i <= N; i++){
-           st = new StringTokenizer(br.readLine());
-           T[i] = Integer.parseInt(st.nextToken());
-           P[i] = Integer.parseInt(st.nextToken());
+        int[] t = new int[n+1];
+        int[] p = new int[n+1];
+        int[] dp = new int[n+2];
+        for(int i=1;i<=n;i++){
+            st = new StringTokenizer(br.readLine());
+            t[i] = Integer.parseInt(st.nextToken());
+            p[i] = Integer.parseInt(st.nextToken());
         }
-
-        for(int i=N;i>0;i--){
-            int next = i+T[i];
-
-            if(next > N+1){
-                dp[i] = dp[i + 1];
+        
+        for(int i=n;i>0;i--){
+            int next = i+t[i];
+            
+            if(next>n+1){
+                dp[i] = dp[i+1];
             }else{
-                dp[i] = Math.max(dp[i+1], P[i]+dp[next]);
+                dp[i] = Math.max(dp[i+1],dp[next]+p[i]);
             }
         }
-
         System.out.println(dp[1]);
     }
 }
