a5d0d488ca70ee46c6c89de992c97b5a788b5faf
ssooomm <101492513+ssooomm@users.noreply.github.com>
2025-05-08 17:02:03 +0900

    [level 2] Title: 괄호 회전하기, Time: 36.04 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -4,40 +4,31 @@ class Solution {
     public int solution(String s) {
         int answer = 0;
         int len = s.length();
-        
-        String ext = s+s;
-        
-        for(int i=0;i<ext.length()-len;i++){
-            if(isStack(ext.substring(i,i+len))){
-                answer++;
+        String str = s+s;
+        for(int i=0;i<len;i++){
+            ArrayDeque<Character> stack= new ArrayDeque<>();
+            boolean flag = true;
+            for(int j=i;j<i+len;j++){
+                char ch = str.charAt(j);
+                if(ch=='['||ch=='{'||ch=='('){
+                    stack.push(ch);
+                }else{
+                    if(stack.isEmpty()){
+                        flag = false;
+                        break;
+                    }
+                    if(stack.peek()=='['&&ch==']') stack.pop();
+                    else if(stack.peek()=='{'&&ch=='}') stack.pop();
+                    else if(stack.peek()=='('&&ch==')') stack.pop();
+                    else{
+                        flag = false;
+                        break;
+                    }
+                }
             }
+            if(flag && stack.isEmpty()) answer++;
         }
+        
         return answer;
     }
-
-    public boolean isStack(String str){
-        char[] arr = str.toCharArray();
-        Deque<Character> stack = new ArrayDeque<>();
-        for(int j=0;j<arr.length;j++){
-            if(arr[j]=='('||arr[j]=='['||arr[j]=='{'){
-                stack.push(arr[j]);
-            }else{
-                if(stack.isEmpty()) return false;
-
-                if(arr[j]==')'&&stack.peek()=='('){
-                    stack.pop();
-                }
-                else if(arr[j]=='}'&&stack.peek()=='{'){
-                    stack.pop();
-                }
-                else if(arr[j]==']'&&stack.peek()=='['){
-                    stack.pop();
-                }
-                else return false;
-            }
-        }
-        return stack.isEmpty();
-        
-    }
-    
 }
