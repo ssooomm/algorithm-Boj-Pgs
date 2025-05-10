0e400efc1656a56ac8802a8741b58a46230ea1a9
ssooomm <101492513+ssooomm@users.noreply.github.com>
2025-05-11 03:47:41 +0900

    [level 2] Title: 소수 찾기, Time: 25.15 ms, Memory: 94.3 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -2,40 +2,39 @@ import java.util.*;
 
 class Solution {
     static Set<Integer> set = new HashSet<>();
-    static char[] arr;
+    static String[] arr;
     static boolean[] visited;
     public int solution(String numbers) {
         int answer = 0;
-        arr = numbers.toCharArray();
+        arr = numbers.split("");
         visited = new boolean[arr.length];
         
-        backtrack(0,0);
-        for(int x:set){
-            if(isPrime(x)) answer++;
+        for(int i=1;i<=arr.length;i++){
+            backtrack(i, "");
+        }
+        for(int s:set){
+            if(isPrime(s)) answer++;
         }
         return answer;
     }
     
-    public void backtrack(int len,int val){
-        if(len==arr.length){
-            return;
+    public void backtrack(int len, String tmp){
+        if(tmp.length()==len){
+            set.add(Integer.parseInt(tmp));
         }
-        
         for(int i=0;i<arr.length;i++){
             if(!visited[i]){
-                int newVal = val + (int)((arr[i]-'0')*Math.pow(10,len));
-                set.add(newVal);
                 visited[i] = true;
-                backtrack(len+1,newVal);
+                backtrack(len,tmp+arr[i]);
                 visited[i] = false;
             }
         }
     }
     
     public boolean isPrime(int n){
-        if(n==1 || n==0) return false;
-        int tmp = (int)Math.sqrt(n);
-        for(int i=2;i<=tmp;i++){
+        if(n==0||n==1) return false;
+        int sq = (int)Math.sqrt(n);
+        for(int i=2;i<=sq;i++){
             if(n%i==0) return false;
         }
         return true;
