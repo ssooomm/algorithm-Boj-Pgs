6baca44bbcd4e2c2f5fb7fe1f3dc243cf20c3147
ssooomm <101492513+ssooomm@users.noreply.github.com>
2025-05-11 09:44:28 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 600 ms, Memory: 142192 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -2,44 +2,39 @@ import java.util.*;
 import java.io.*;
 
 public class Main{
-    static int cnt=0;
-    static boolean[] visited;
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-
-        List<List<Integer>> list = new ArrayList<>();
-        for(int i=0;i<=n;i++){
-            list.add(new ArrayList<>());
+        List<Integer>[] list = new ArrayList[n+1];
+        for(int i=1;i<=n;i++){
+            list[i] = new ArrayList<>();
         }
         for(int i=0;i<m;i++){
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
-            list.get(u).add(v);
-            list.get(v).add(u);
+            list[u].add(v);
+            list[v].add(u);
         }
-
-        visited = new boolean[n+1];
-        for(int x=1;x<=n;x++){
-            if(!visited[x]){
-                cnt++;
-                visited[x] = true;
-                dfs(list,list.get(x));
-            }
-        }
-
-        System.out.println(cnt);
-    }
-
-    static void dfs(List<List<Integer>> list, List<Integer> tmp){
-
-        for(int i:tmp){
+        boolean[] visited = new boolean[n+1];
+        int cnt =0;
+        for(int i=1;i<=n;i++){
             if(!visited[i]){
                 visited[i] = true;
-                dfs(list,list.get(i));
+               backtrack(list,visited,list[i]);
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+    
+    static void backtrack(List<Integer>[] list, boolean[] visited,List<Integer> cur){
+        for(int c:cur){
+            if(!visited[c]){
+                visited[c] = true;
+                backtrack(list,visited,list[c]);
             }
         }
     }
