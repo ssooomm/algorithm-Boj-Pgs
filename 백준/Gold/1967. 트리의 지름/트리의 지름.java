e26b5525f8e7fb20a0d4221ce4ac76f30de8466b
ssooomm <101492513+ssooomm@users.noreply.github.com>
2025-05-13 04:15:44 +0900

    [Gold IV] Title: 트리의 지름, Time: 220 ms, Memory: 24880 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -1,58 +1,62 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 class Node{
-    int value;
+    int val;
     int cost;
-    public Node(int value, int cost){
-        this.value = value;
+    Node(int val, int cost){
+        this.val = val;
         this.cost = cost;
     }
 }
-public class Main {
-    static int max_idx = 0;
-    static int max = 0;
+
+public class Main{
     static boolean[] visited;
+    static int max_idx=0,max=0;
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-
-        Map<Integer, List<Node>> tree = new HashMap<>();
-        for(int i=0;i<=N;i++){
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        
+        Map<Integer,List<Node>> tree = new HashMap<>();
+        for(int i=0;i<=n;i++){
             tree.put(i,new ArrayList<>());
         }
-
-        for(int i=0;i<N-1;i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        for(int i=0;i<n-1;i++){
+            st = new StringTokenizer(br.readLine());
             int n1 = Integer.parseInt(st.nextToken());
             int n2 = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
-
-            tree.get(n1).add(new Node(n2, c));
+            
+            tree.get(n1).add(new Node(n2,c));
             tree.get(n2).add(new Node(n1, c));
         }
-
-        visited = new boolean[N+1];
+        
+        //부모로부터 가장 가중치 큰 노드 찾기
+        visited = new boolean[n+1];
         visited[1] = true;
         dfs(tree,1,0);
-
-        visited = new boolean[N+1];
+        
+        //그 노드로부터 가장 가중치 큰 게 큰 지름 
+        visited = new boolean[n+1];
         visited[max_idx] = true;
         dfs(tree,max_idx,0);
+        
         System.out.println(max);
-
+        
     }
-
-    static void dfs(Map<Integer, List<Node>> tree, int idx, int cost){
+    
+    static void dfs(Map<Integer,List<Node>> tree, int idx, int cost){
         if(max<cost){
             max = cost;
             max_idx = idx;
         }
-
+        
         for(Node next:tree.get(idx)){
-            if(!visited[next.value]){
-                visited[next.value] = true;
-                dfs(tree, next.value,cost+next.cost);
+            if(!visited[next.val]){
+                visited[next.val] = true;
+                dfs(tree,next.val,cost+next.cost);
             }
         }
     }
