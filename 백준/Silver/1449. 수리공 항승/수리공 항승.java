70d3ed0d62761497d8038e26a351b055ead2bc7d
ssooomm <101492513+ssooomm@users.noreply.github.com>
2025-05-12 18:24:38 +0900

    [Silver III] Title: 수리공 항승, Time: 104 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/1449. 수리공 항승/수리공 항승.java
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.java
@@ -1,22 +1,27 @@
 import java.util.*;
+import java.io.*;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int L = sc.nextInt();
-        int[] arr = new int[N];
-        for(int i=0;i<N;i++){
-            arr[i] = sc.nextInt();
-        }
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int l = Integer.parseInt(st.nextToken());
         
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
         Arrays.sort(arr);
-        double tmp=0;
-        int cnt=0;
-        for(int i=0;i<N;i++){
-            if(tmp>arr[i]) continue;
-            tmp = arr[i]+L-0.5;
-            cnt++;
+        
+        int len = 0, cnt =0 ;
+        for(int i=0;i<n;i++){
+            if(arr[i]<=len) continue;
+            else{
+                cnt++;
+                len = arr[i] + l - 1;
+            }
         }
         System.out.println(cnt);
     }
