75f698388841e4af6133bd72104b08653dbdc6b2
ssooomm <101492513+ssooomm@users.noreply.github.com>
2025-05-15 00:29:24 +0900

    [Silver V] Title: 덩치, Time: 116 ms, Memory: 15912 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -1,31 +1,30 @@
 import java.util.*;
+import java.io.*;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
         int[][] arr = new int[n][2];
-        int[] ans = new int[n];
         
         for(int i=0;i<n;i++){
-            arr[i][0] = sc.nextInt();
-            arr[i][1] = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
         
-        StringBuilder sb= new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         for(int i=0;i<n;i++){
-            int x = arr[i][0];
-            int y = arr[i][1];
-            int cnt = 1;
+            int cnt=1;
             for(int j=0;j<n;j++){
                 if(i==j) continue;
-                if(arr[j][0]>x && arr[j][1]>y){
-                    cnt++;
-                }
+                if(arr[j][0]>arr[i][0]&&arr[j][1]>arr[i][1]) cnt++;
             }
             sb.append(cnt+" ");
         }
         
         System.out.println(sb);
     }
+    
 }
