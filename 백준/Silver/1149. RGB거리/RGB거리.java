45c8255cf9a79242506dcbe9210cf5a5605d7f4c
ssooomm <101492513+ssooomm@users.noreply.github.com>
2025-05-15 15:40:42 +0900

    [Silver I] Title: RGB거리, Time: 108 ms, Memory: 14504 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,35 +1,28 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class Main{
+    public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[][] arr = new int[N+1][3];
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][3];
+        int[][] dp = new int[n][3];
         StringTokenizer st;
-        for (int i = 1; i <= N; i++) {
+        for(int i=0;i<n;i++){
             st = new StringTokenizer(br.readLine());
             arr[i][0] = Integer.parseInt(st.nextToken());
             arr[i][1] = Integer.parseInt(st.nextToken());
             arr[i][2] = Integer.parseInt(st.nextToken());
         }
-
-//        int N = 3;
-//        int[][]arr = {
-//                {0,0,0},
-//                {26, 40, 83},
-//                {49, 60, 57},
-//                {13, 89, 99}
-//        };
-
-        int[][] d = new int[N+1][3];
-        for(int i=1;i<=N;i++){
-            d[i][0] = Math.min(d[i-1][1],d[i-1][2]) + arr[i][0];
-            d[i][1] = Math.min(d[i-1][0],d[i-1][2]) + arr[i][1];
-            d[i][2] = Math.min(d[i-1][0],d[i-1][1]) + arr[i][2];
+        dp[n-1][0] = arr[n-1][0];
+        dp[n-1][1] = arr[n-1][1];
+        dp[n-1][2] = arr[n-1][2];
+        for(int i=n-2;i>=0;i--){
+            dp[i][0] = Math.min(dp[i+1][1],dp[i+1][2])+arr[i][0];
+            dp[i][1] = Math.min(dp[i+1][0],dp[i+1][2])+arr[i][1];
+            dp[i][2] = Math.min(dp[i+1][1],dp[i+1][0])+arr[i][2];
         }
-        System.out.println(Arrays.stream(d[N]).min().getAsInt());
+        int min = Math.min(dp[0][0],Math.min(dp[0][1],dp[0][2]));
+        System.out.println(min);
     }
 }
