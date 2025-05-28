c7f635581a1757255309c5d07c19a2726ffda24a
ssooomm <101492513+ssooomm@users.noreply.github.com>
2025-05-29 00:57:22 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 536 ms, Memory: 49636 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,69 +1,60 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-	static int total;
-	static List<Node>[] list;
-	static boolean[] visited;
-	static class Node implements Comparable<Node>{
-		int to;
-		int value;
-		
-		public Node(int to, int value) {
-			this.to = to;
-			this.value = value;
-		}
+public class Main{
+    static int[] pt; // 부모 저장 
+    static class Node implements Comparable<Node>{
+        int to;
+        int from;
+        int c;
+        public Node(int to, int from,int c){
+            this.to=to;
+            this.from = from;
+            this.c=c;
+        }
 
-		@Override
-		public int compareTo(Node o) {
-			return this.value - o.value;
-		}
-	}
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int v = Integer.parseInt(st.nextToken());
-		int e = Integer.parseInt(st.nextToken());
-		
-		list = new ArrayList[v+1];
-		visited = new boolean[v+1];
-		for(int i=1; i<v+1; i++) {
-			list[i] = new ArrayList<>();
-		}
-		
-		for(int i=0; i<e; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
-			list[a].add(new Node(b,w));
-			list[b].add(new Node(a,w));
-		}
-		
-		prim(1);
-		System.out.println(total);
-	}
-	
-	static void prim(int start) {
-		Queue<Node> pq = new PriorityQueue<>();
-		
-		pq.add(new Node(start,0));
-		while(!pq.isEmpty()) {
-			Node p = pq.poll();
-			int node = p.to;
-			int weight = p.value;
-			
-			if(visited[node]) continue;
-			visited[node]= true;
-			total += weight;
-			
-			for(Node next : list[node]) {
-				if(!visited[next.to]) {
-					pq.add(next);
-				}
-			}
-		}
-		
-	}
+        @Override
+        public int compareTo(Node n){
+            return this.c-n.c;
+        }
+    }
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int v = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        
+        pt = new int[v+1];
+        for(int i=1;i<=v;i++){
+            pt[i] = i;
+        }
+        
+        Queue<Node> pq = new PriorityQueue<>();
+        for(int i=0;i<e;i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            pq.add(new Node(a,b,c));
+        }
+        
+        int res=0;
+        while(!pq.isEmpty()){
+            Node cur = pq.poll();
+            int to = find(cur.to);
+            int from = find(cur.from);
+            if(to!=from){
+                res += cur.c;
+                pt[from] = to;
+            }
+        }
+       System.out.println(res);
+    }
+    
+    //부모 노드 찾기
+    static int find(int a){
+        if(pt[a]==a) return a;
+        return pt[a] = find(pt[a]); //pt값 갱신하면서 경로 압축 
+    }
+    
 }
