0a4e6d9ed61f1b816ae735b16f6704c4e25dab42
ssooomm <101492513+ssooomm@users.noreply.github.com>
2025-06-09 15:03:30 +0900

    [Silver I] Title: 경로 찾기, Time: 212 ms, Memory: 20300 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -1,60 +1,52 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-
-    static int[][] matrix;
-    static Map<Integer,List<Integer>> graph;
-
-    public static void main(String[] args) throws IOException{
+public class Main{
+    public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
-        int N = Integer.parseInt(br.readLine());
-        matrix = new int[N][N];
-        graph = new HashMap<>();
-
-        for(int i=0;i<N;i++){
-            graph.put(i,new ArrayList<>());
-            String[] row = br.readLine().split(" ");
-            for(int j=0;j<N;j++){
-                if(Integer.parseInt(row[j])==1){
-                    graph.get(i).add(j);
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        
+        List<List<Integer>> list = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            list.add(new ArrayList<>());
+        }
+        for(int i=0;i<n;i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0;j<n;j++){
+                int x = Integer.parseInt(st.nextToken());
+                if(x==1){
+                    list.get(i).add(j);
                 }
             }
         }
-
-        // 각 정점에서 탐색 진행
-        for(int i=0;i<N;i++){
-            bfs(i,N);
+        
+        int[][] arr = new int[n][n];
+        Queue<Integer> q;
+        boolean[] visited;
+        for(int i=0;i<n;i++){
+            q = new ArrayDeque<>();
+            q.add(i);
+            visited = new boolean[n];
+            while(!q.isEmpty()){
+                int cur = q.poll();
+                for(int l:list.get(cur)){
+                    if(!visited[l]){
+                        arr[i][l] = 1;
+                        q.add(l);
+                        visited[l] = true;
+                    }
+                }
+            }
         }
-        for(int i=0;i<N;i++){
-            for(int j=0;j<N;j++){
-                sb.append(matrix[i][j]+" ");
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                sb.append(arr[i][j]+" ");
             }
             sb.append("\n");
         }
-
         System.out.println(sb);
-
-
     }
-
-    public static void bfs(int st, int N){
-        Queue<Integer> q = new ArrayDeque<>();
-        boolean[] visited = new boolean[N];
-        q.add(st);
-        while(!q.isEmpty()){
-            int curr = q.remove();
-            for(Integer next:graph.get(curr)){
-                if(!visited[next]){
-                    visited[next] = true;
-                    matrix[st][next] = 1;
-                    q.add(next);
-                }
-            }
-        }
-    }
-
-
 }
