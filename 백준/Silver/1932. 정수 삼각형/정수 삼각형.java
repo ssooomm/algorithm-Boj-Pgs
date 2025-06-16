98cfca300855825bb156c95def4ee6a9fc86479f
ssooomm <101492513+ssooomm@users.noreply.github.com>
2025-06-16 21:19:49 +0900

    [Silver I] Title: 정수 삼각형, Time: 236 ms, Memory: 25576 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -13,16 +13,12 @@ public class Main{
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        
-        int[][] dp = new int[n][n];
-        for(int i=0;i<n;i++){
-            dp[n-1][i] = arr[n-1][i];
-        }
+
         for(int i=n-2;i>=0;i--){
             for(int j=0;j<=i;j++){
-                dp[i][j] += arr[i][j] + Math.max(dp[i+1][j],dp[i+1][j+1]);
+                arr[i][j] += Math.max(arr[i+1][j],arr[i+1][j+1]);
             }
         }
-        System.out.println(dp[0][0]);
+        System.out.println(arr[0][0]);
     }
 }
