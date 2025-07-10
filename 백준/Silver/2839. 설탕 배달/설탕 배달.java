867e6bf33487ff4d1357f749c3029400d9df2725
ssooomm <101492513+ssooomm@users.noreply.github.com>
2025-07-10 12:58:45 +0900

    [Silver IV] Title: 설탕 배달, Time: 176 ms, Memory: 17684 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -4,18 +4,13 @@ public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int res=0;
-        
-        while(n>=0){
-            if(n%5==0){
-                res+=n/5;
-                System.out.println(res);
-                return;
-            }
-            n-=3;
-            res++;
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,5001);
+        dp[0] = 0;
+        for(int i=1;i<=n;i++){
+            if(i>=3) dp[i] = Math.min(dp[i],dp[i-3]+1);
+            if(i>=5) dp[i] = Math.min(dp[i],dp[i-5]+1);
         }
-        System.out.println(-1);
-        return;
+        System.out.println(dp[n]>=5001?-1:dp[n]);
     }
 }
