7ec73144060c8cf5f0b2d69ee52ed4d3d21cffe6
ssooomm <101492513+ssooomm@users.noreply.github.com>
2025-07-21 18:28:47 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 172 ms, Memory: 17636 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -4,20 +4,16 @@ public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
+        int[] arr = new int[12];
+        arr[1] = 1;
+        arr[2] = 2;
+        arr[3] = 4;
+        for(int i=4;i<=11;i++){
+            arr[i] = arr[i-1]+arr[i-2]+arr[i-3];
+        }
         while(t-->0){
             int n = sc.nextInt();
-            int[] dp = new int[11];
-            
-            dp[1] = 1;
-            dp[2] = 2;
-            dp[3] = 4;
-            
-            for(int i=4;i<=n;i++){
-                dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
-            }
-            
-            System.out.println(dp[n]);
+            System.out.println(arr[n]);
         }
     }
-
 }
