fa2e85c6c8d907ba7efdc31765f5a73d479a821a
ssooomm <101492513+ssooomm@users.noreply.github.com>
2025-07-25 18:44:23 +0900

    [Silver I] Title: 동물원, Time: 196 ms, Memory: 21156 KB -BaekjoonHub

---
+++ 백준/Silver/1309. 동물원/동물원.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] dp = new int[n+1][3];
+        dp[1][0] = 1;
+        dp[1][1] = 1;
+        dp[1][2] = 1;
+        for(int i=2;i<=n;i++){
+            dp[i][0] = (dp[i-1][0]+dp[i-1][1]+dp[i-1][2])%9901; //x
+            dp[i][1] = (dp[i-1][0]+dp[i-1][2])%9901; //왼
+            dp[i][2] = (dp[i-1][0]+dp[i-1][1])%9901; //오
+        }
+        System.out.println((dp[n][0]+dp[n][1]+dp[n][2])%9901);
+    }
+}
