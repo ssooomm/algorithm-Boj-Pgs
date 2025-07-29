14e658dee6558a1ba44764dd7d008c904b641084
ssooomm <101492513+ssooomm@users.noreply.github.com>
2025-07-29 13:19:29 +0900

    [Silver I] Title: 포도주 시식, Time: 128 ms, Memory: 14936 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -1,20 +1,23 @@
 import java.util.*;
+import java.io.*;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n+1];
         for(int i=1;i<=n;i++){
-            arr[i] = sc.nextInt();
+            arr[i] = Integer.parseInt(br.readLine());
         }
-        
         int[] dp = new int[n+1];
         dp[1] = arr[1];
-        if(n>=2) dp[2] = arr[1] + arr[2];
+        if(n>=2) dp[2] = arr[1]+arr[2];
+        
         for(int i=3;i<=n;i++){
-            dp[i] = Math.max(dp[i-1],Math.max(dp[i-3]+arr[i-1],dp[i-2])+arr[i]);
+            int tmp = Math.max(dp[i-3]+arr[i-1],dp[i-2])+arr[i];
+            dp[i] = Math.max(dp[i-1],tmp);
         }
+        
         System.out.println(dp[n]);
     }
 }
