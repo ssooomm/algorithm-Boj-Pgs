457f96254fa34eb84482628dc749cc394e904dc7
ssooomm <101492513+ssooomm@users.noreply.github.com>
2025-08-01 14:56:40 +0900

    [Gold IV] Title: 전화번호 목록, Time: 620 ms, Memory: 31740 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -2,7 +2,6 @@ import java.util.*;
 import java.io.*;
 
 public class Main{
-    static Set<String> set;
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
@@ -13,29 +12,18 @@ public class Main{
             for(int i=0;i<n;i++){
                 str[i] = br.readLine();
             }
-            Arrays.sort(str,(a,b)->a.length()-b.length());
-            set = new HashSet<>();
-            set.add(str[0]);
-            boolean flag = true;
-            for(int i=1;i<n;i++){
-                if(check(str[i])){
-                    set.add(str[i]);
-                }else{
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag) res.append("YES\n");
+            Arrays.sort(str); //911, 911345
+            if(check(str)){
+                res.append("YES\n");
+            } 
             else res.append("NO\n");
         }
         System.out.println(res);
     }
     
-    static boolean check(String str){
-        StringBuilder sb = new StringBuilder();
-        for(int x=0;x<str.length();x++){
-            sb.append(str.charAt(x));
-            if(set.contains(sb.toString())){
+    static boolean check(String[] str){
+        for(int x=0;x<str.length-1;x++){
+            if(str[x+1].startsWith(str[x])){
                 return false;
             }
         }
