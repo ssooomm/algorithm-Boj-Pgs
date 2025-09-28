7c9bc913a96a1d05ed959ef3e6765f27dfa3f322
ssooomm <101492513+ssooomm@users.noreply.github.com>
2025-09-28 12:04:36 +0900

    [Silver I] Title: 신입 사원, Time: 788 ms, Memory: 298768 KB -BaekjoonHub

---
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		int tc = Integer.parseInt(br.readLine());
+		while(tc-->0) {
+			int n = Integer.parseInt(br.readLine());
+			
+			int[] arr = new int[n+1];
+			for(int i=0; i<n; i++) {
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				arr[a] = b;
+			}
+			int count =1;
+			int rating = arr[1];
+			for(int i=2; i<=n; i++) {
+				if(rating>arr[i]) {
+					count++;
+					rating = arr[i];
+				}
+			}
+			sb.append(count+"\n");
+		}
+		System.out.println(sb.toString());
+	}
+}
