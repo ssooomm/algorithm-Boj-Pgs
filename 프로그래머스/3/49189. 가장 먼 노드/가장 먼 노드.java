1b95087e0eb52f2400daccf32c59b36016a0eb77
ssooomm <101492513+ssooomm@users.noreply.github.com>
2025-09-28 11:36:07 +0900

    [level 3] Title: 가장 먼 노드, Time: 26.41 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -2,30 +2,33 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] edge) {
+        int answer = 0;
         List<Integer>[] list = new ArrayList[n+1];
-        for(int i=1;i<=n;i++){
+        for(int i=0;i<=n;i++){
             list[i] = new ArrayList<>();
         }
         for(int[] e:edge){
             list[e[0]].add(e[1]);
             list[e[1]].add(e[0]);
         }
-        int max = 0, cnt = 0;
-        boolean[] visited = new boolean[n+1];
+        
+        int max=0,cnt=0;
+        boolean[] v = new boolean[n+1];
         Queue<int[]> q = new ArrayDeque<>();
         q.add(new int[]{1,0});
-        visited[1] = true;
+        v[1] = true;
         
         while(!q.isEmpty()){
-            int[] cur = q.remove();
-            if(cur[1]>max){
-                cnt = 1;
-                max = cur[1];
-            }else if(cur[1] == max) cnt++;
-            for(int x:list[cur[0]]){
-                if(!visited[x]){
-                    q.add(new int[]{x,cur[1]+1});
-                    visited[x] = true;
+            int[] tmp = q.poll();
+            if(tmp[1]>max){
+                cnt=1;
+                max = tmp[1];
+            }else if(tmp[1]==max) cnt++;
+            
+            for(int x:list[tmp[0]]){
+                if(!v[x]){
+                    v[x] = true;
+                    q.add(new int[]{x,tmp[1]+1});
                 }
             }
         }
