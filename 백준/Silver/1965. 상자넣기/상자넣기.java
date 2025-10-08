372a4aa4143de3dba2d2ea4331680f6997835059
ssooomm <101492513+ssooomm@users.noreply.github.com>
2025-10-08 23:01:04 +0900

    [Silver II] Title: 상자넣기, Time: 212 ms, Memory: 18532 KB -BaekjoonHub

--- 백준/Silver/1965. 상자넣기/상자넣기.java
+++ 백준/Silver/1965. 상자넣기/상자넣기.java
@@ -5,23 +5,32 @@ public class Main{
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
-        int[] dp = new int[n];
+        
         for(int i=0;i<n;i++){
             arr[i] = sc.nextInt();
         }
         
-        int max = 0;
-        for(int i=0;i<n;i++){
-            dp[i]=1;
-            for(int j=0;j<i;j++){
-                if(arr[i]>arr[j]){
-                    dp[i] = Math.max(dp[i],dp[j]+1);
-                    
+        List<Integer> list = new ArrayList<>();
+        list.add(arr[0]);
+        
+        for(int i=1;i<n;i++){
+            if(list.get(list.size()-1)<arr[i]){
+                list.add(arr[i]);
+            }else{
+                int st = 0;
+                int end = list.size()-1;
+                while(st<end){
+                    int mid = (st+end)/2;
+                    if(list.get(mid)<arr[i]){
+                        st = mid+1;
+                    }else{
+                        end=mid;
+                    }
                 }
+                list.set(end,arr[i]);
             }
-            max = Math.max(max,dp[i]);
         }
         
-        System.out.println(max);
+        System.out.println(list.size());
     }
 }
