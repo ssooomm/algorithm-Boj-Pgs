e37e92fb7bf1cdda5aa55535cfb5085bf08e9c88
ssooomm <101492513+ssooomm@users.noreply.github.com>
2025-10-21 10:18:05 +0900

    [Silver II] Title: 그르다 김가놈, Time: 556 ms, Memory: 93824 KB -BaekjoonHub

--- 백준/Silver/18113. 그르다 김가놈/그르다 김가놈.java
+++ 백준/Silver/18113. 그르다 김가놈/그르다 김가놈.java
@@ -2,45 +2,44 @@ import java.util.*;
 import java.io.*;
 
 public class Main{
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-
-        //김밥 자르기
         List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            int x = Integer.parseInt(br.readLine());
-            if(x<=k) continue;
-            else if(x<2*k) list.add(x - k);
-            else list.add(x - (2 * k));
-        }
-
-        int res = -1;
-        int start = 1;
-        int end = 0;
-        for (int x : list) {
-            end = Math.max(x, end);
-        }
-
-        while (start <= end) {
-            int mid = (end + start) / 2;
-            int sum = 0;
-            for (int x : list) {
-                sum += x / mid;
+        int max=0;
+        for(int i=0;i<n;i++){
+            int l = Integer.parseInt(br.readLine());
+            if(l<=k) continue;
+            if(l<2*k){
+                l-=k;
             }
-
-            //더 길게 자를 수 있는지 범위 조정
-            if (sum >= m) {
+            else{
+                l-=k*2;
+            }
+            if(l==0) continue;
+            list.add(l);
+            max = Math.max(l,max);
+        }
+        int sta = 1;
+        int end = max;
+        int res=-1;
+        while(sta<=end){
+            int mid = (sta+end)/2;
+            int cnt=0;
+            for(int i:list){
+                cnt+=i/mid;
+            }
+            if(cnt>=m){
+                sta=mid+1;
                 res = mid;
-                start = mid + 1;
-            } else {
-                end = mid - 1;
+            }else{
+                end=mid-1;
             }
         }
-
+        
         System.out.println(res);
     }
 }
