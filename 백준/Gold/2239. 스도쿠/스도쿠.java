647fa4367183d5e9b86d1fcc6ef5622f96961457
ssooomm <101492513+ssooomm@users.noreply.github.com>
2025-10-30 11:06:44 +0900

    [Gold IV] Title: 스도쿠, Time: 592 ms, Memory: 22808 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.java
+++ 백준/Gold/2239. 스도쿠/스도쿠.java
@@ -2,16 +2,16 @@ import java.util.*;
 
 public class Main{
     static int[][] map = new int[9][9];
-    static boolean isSolved = false; //정답 찾은 후 재귀 그만 
+    static boolean isP = true;
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        
         for(int i=0;i<9;i++){
             String str = sc.next();
             for(int j=0;j<9;j++){
                 map[i][j] = str.charAt(j)-'0';
             }
         }
+        
         bt(0,0);
         
         for(int i=0;i<9;i++){
@@ -23,53 +23,41 @@ public class Main{
     }
     
     static void bt(int r, int c){
-        //이미 답 찾음 
-        if(isSolved) return;
+        if(!isP) return;
         
-        //다음 행으로 
         if(c==9){
             r++;
             c=0;
         }
         
-        // 행 끝까지 오면 스도쿠 완성 
         if(r==9){
-            isSolved = true;
+            isP = false;
             return;
         }
         
-        if(map[r][c]==0){ //채우기
-            for(int n=1;n<=9;n++){ //사전순 1~9
-                if(isP(r,c,n)){
-                    map[r][c]=n; //대입
-                    bt(r,c+1); //한 칸 옆으로
-                    
-                    //답 찾음
-                    if(isSolved) return;
-                    
-                    //답 못 찾음 -> 채워준 칸 없애기
+        if(map[r][c]==0){
+            for(int i=1;i<=9;i++){
+                if(check(r,c,i)){
+                    map[r][c] = i;
+                    bt(r,c+1);
+
+                    if(!isP) return;
                     map[r][c] = 0;
                 }
             }
-        }
-        else{
-            //이미 채워진 칸
+        }else{
             bt(r,c+1);
         }
     }
     
-    static boolean isP(int r,int c, int n){
-        //가로 검사
+    static boolean check(int r,int c,int n){
+        for(int i=0;i<9;i++){
+            if(map[i][c]==n) return false;
+        }
         for(int i=0;i<9;i++){
             if(map[r][i]==n) return false;
         }
         
-        //세로 검사
-        for(int i=0;i<9;i++){
-            if(map[i][c]==n) return false;
-        }
-        
-        //3*3
         int nr = (r/3)*3;
         int nc = (c/3)*3;
         for(int i=nr;i<nr+3;i++){
@@ -77,6 +65,7 @@ public class Main{
                 if(map[i][j]==n) return false;
             }
         }
+        
         return true;
     }
 }
