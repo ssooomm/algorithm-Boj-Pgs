e140b08ea64d8e74949a947b72aae4e87b257738
ssooomm <101492513+ssooomm@users.noreply.github.com>
2025-11-07 16:21:17 +0900

    [level 2] Title: 괄호 회전하기, Time: 32.54 ms, Memory: 93.3 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -3,32 +3,37 @@ import java.util.*;
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        int len = s.length();
-        String str = s+s;
-        for(int i=0;i<len;i++){
-            ArrayDeque<Character> stack= new ArrayDeque<>();
-            boolean flag = true;
-            for(int j=i;j<i+len;j++){
-                char ch = str.charAt(j);
-                if(ch=='['||ch=='{'||ch=='('){
-                    stack.push(ch);
-                }else{
-                    if(stack.isEmpty()){
-                        flag = false;
-                        break;
-                    }
-                    if(stack.peek()=='['&&ch==']') stack.pop();
-                    else if(stack.peek()=='{'&&ch=='}') stack.pop();
-                    else if(stack.peek()=='('&&ch==')') stack.pop();
-                    else{
-                        flag = false;
-                        break;
-                    }
+        String ss = s+s;
+        for(int i=0;i<s.length();i++){
+            if(check(ss.substring(i,i+s.length()))){
+                answer++;
+            }
+        }
+        return answer;
+    }
+    
+    boolean check(String str){
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+        boolean flag = true;
+        for(int i=0;i<str.length();i++){
+            char ch = str.charAt(i);
+            if(ch=='('||ch=='{'||ch=='[') stack.push(ch);
+            else{
+                if(stack.isEmpty()){
+                    flag = false;
+                    break;
+                }
+                if(stack.peek()=='['&&ch==']') stack.pop();
+                else if(stack.peek()=='{'&&ch=='}') stack.pop();
+                else if(stack.peek()=='('&&ch==')') stack.pop();
+                else{
+                    flag = false;
+                    break;
                 }
             }
-            if(flag && stack.isEmpty()) answer++;
+            
         }
-        
-        return answer;
+        if(stack.isEmpty()&&flag) return true;
+        return false;
     }
 }
