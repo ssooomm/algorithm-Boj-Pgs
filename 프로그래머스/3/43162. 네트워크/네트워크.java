641673b5a3c3b198ca2c2dfec3469f8b730ceda6
ssooomm <101492513+ssooomm@users.noreply.github.com>
2025-11-07 14:59:11 +0900

    [level 3] Title: 네트워크, Time: 1.17 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,26 +1,49 @@
+import java.util.*;
+
 class Solution {
+    static boolean[] v;
+    static List<Integer>[] list;
     public int solution(int n, int[][] computers) {
         int answer = 0;
+        v = new boolean[n+1];
         
-        boolean[] visited = new boolean[n];
+        list = new ArrayList[n+1];
+        for(int i=1;i<=n;i++){
+            list[i] = new ArrayList<>();
+        }
         
         for(int i=0;i<n;i++){
-            if(!visited[i]){
-                dfs(i,visited,computers,n);
-                answer++;
+            for(int j=0;j<n;j++){
+                if(i==j) continue;
+                if(computers[i][j]==1) {
+                    list[i+1].add(j+1);
+                    list[j+1].add(i+1);
+                }
             }
         }
         
-        return answer;
+        int cnt=0;
+        for(int i=1;i<=n;i++){
+            if(!v[i]){
+                v[i] = true;
+                bfs(i);
+                cnt++;
+            }
+        }
+        return cnt;
     }
     
-    public void dfs(int i, boolean[] visited, int[][] computers, int n){
-        visited[i] = true;
-        for(int j=0;j<n;j++){
-            if(!visited[j] && computers[i][j]==1){
-                dfs(j,visited,computers,n);
+    static void bfs(int i){
+        Queue<Integer> q = new ArrayDeque<>();
+        q.add(i);
+        while(!q.isEmpty()){
+            int tmp = q.poll();
+            for(int l:list[tmp]){
+                if(!v[l]){
+                    v[l] = true;
+                    q.add(l);
+                }
             }
         }
     }
-    
 }
