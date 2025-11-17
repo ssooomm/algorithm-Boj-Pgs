ca56c216bd76d21e7b1eb9d0bad2776e76cd0e4a
ssooomm <101492513+ssooomm@users.noreply.github.com>
2025-11-17 22:45:53 +0900

    [level 2] Title: 괄호 회전하기, Time: 29.44 ms, Memory: 92.3 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -3,37 +3,35 @@ import java.util.*;
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        String ss = s+s;
+        String str = s+s;
+        
         for(int i=0;i<s.length();i++){
-            if(check(ss.substring(i,i+s.length()))){
-                answer++;
-            }
+            if(check(str.substring(i,s.length()+i))) answer++;
         }
+        
         return answer;
     }
     
     boolean check(String str){
-        ArrayDeque<Character> stack = new ArrayDeque<>();
-        boolean flag = true;
+        Deque<Character> stack = new ArrayDeque<>();
+        
         for(int i=0;i<str.length();i++){
             char ch = str.charAt(i);
-            if(ch=='('||ch=='{'||ch=='[') stack.push(ch);
-            else{
-                if(stack.isEmpty()){
-                    flag = false;
-                    break;
-                }
-                if(stack.peek()=='['&&ch==']') stack.pop();
-                else if(stack.peek()=='{'&&ch=='}') stack.pop();
-                else if(stack.peek()=='('&&ch==')') stack.pop();
-                else{
-                    flag = false;
-                    break;
-                }
+            if(ch=='('||ch=='{'||ch=='['){
+                stack.push(ch);
+            }else{
+                if(stack.isEmpty()) return false;
+                
+                if(ch==')'&&stack.peek()=='(')
+                    stack.pop();
+                else if(ch=='}'&&stack.peek()=='{')
+                    stack.pop();
+                else if(ch==']'&&stack.peek()=='[')
+                    stack.pop();
+                else return false;
             }
-            
         }
-        if(stack.isEmpty()&&flag) return true;
-        return false;
+        if(stack.isEmpty()) return true;
+        else return false;
     }
 }
