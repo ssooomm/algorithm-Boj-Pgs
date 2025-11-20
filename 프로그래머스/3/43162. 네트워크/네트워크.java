5ca24a34a7e37f489da55606f628154ba424dfcc
ssooomm <101492513+ssooomm@users.noreply.github.com>
2025-11-20 22:38:02 +0900

    [level 3] Title: 네트워크, Time: 1.01 ms, Memory: 89.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,49 +1,47 @@
 import java.util.*;
 
 class Solution {
-    static boolean[] v;
-    static List<Integer>[] list;
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        v = new boolean[n+1];
         
-        list = new ArrayList[n+1];
-        for(int i=1;i<=n;i++){
+        boolean[] v = new boolean[n];
+        
+        List<Integer>[] list = new ArrayList[n];
+        for(int i=0;i<n;i++){
             list[i] = new ArrayList<>();
         }
-        
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(i==j) continue;
-                if(computers[i][j]==1) {
-                    list[i+1].add(j+1);
-                    list[j+1].add(i+1);
+                if(j==i) continue;
+                if(computers[i][j]==1){
+                    list[i].add(j);
+                    list[j].add(i);
                 }
             }
         }
         
-        int cnt=0;
-        for(int i=1;i<=n;i++){
-            if(!v[i]){
-                v[i] = true;
-                bfs(i);
-                cnt++;
-            }
-        }
-        return cnt;
-    }
-    
-    static void bfs(int i){
         Queue<Integer> q = new ArrayDeque<>();
-        q.add(i);
-        while(!q.isEmpty()){
-            int tmp = q.poll();
-            for(int l:list[tmp]){
-                if(!v[l]){
-                    v[l] = true;
-                    q.add(l);
+        for(int i=0;i<n;i++){
+            if(!v[i]){
+                answer++;
+                q.add(i);
+                v[i] = true;
+                while(!q.isEmpty()){
+                    int tmp = q.poll();
+                    for(int x:list[tmp]){
+                        if(!v[x]){
+                            v[x] = true;
+                            q.add(x);
+                        }
+                    }
                 }
             }
         }
+        
+        
+        
+        
+        
+        return answer;
     }
 }
