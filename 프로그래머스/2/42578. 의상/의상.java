0f6f0e7e38d386b7f5df39ee0fc9192f9d6c8827
ssooomm <101492513+ssooomm@users.noreply.github.com>
2026-02-03 03:22:33 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -3,14 +3,14 @@ import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
+        Map<String,Integer> hm = new HashMap<>();
+        for(String[] c:clothes){
+            hm.put(c[1],hm.getOrDefault(c[1],0)+1);
+        }
+        for(String k:hm.keySet()){
+            answer*=(hm.get(k)+1);
+        }
         
-        HashMap<String, Integer> hm = new HashMap<>();
-        for (String[] s : clothes) {
-            hm.put(s[1], hm.getOrDefault(s[1],0)+1);
-        }
-        for(Integer v:hm.values()){
-            answer *= v+1;
-        }
         return answer-1;
     }
 }
