5aa0431588cf29d085ecbd98fe707c935dffbd60
ssooomm <101492513+ssooomm@users.noreply.github.com>
2026-02-12 22:47:14 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.55 ms, Memory: 55.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,36 +1,36 @@
 import java.util.*;
 
 class Solution {
-    int[] dx = {1,-1,0,0};
-    int[] dy = {0,0,1,-1};
+    static int[] dx = {1,-1,0,0};
+    static int[] dy = {0,0,1,-1};
+    
     public int solution(int[][] maps) {
-        int answer = -1;
-        int n = maps.length;
-        int m = maps[0].length;
+        int n = maps.length-1;
+        int m = maps[0].length-1;
+        int answer = Integer.MAX_VALUE;
+        boolean[][] v = new boolean[n+1][m+1];
         
         Queue<int[]> q = new ArrayDeque<>();
         q.add(new int[]{0,0,1});
-        boolean[][] v = new boolean[n][m];
         v[0][0] = true;
         
         while(!q.isEmpty()){
             int[] cur = q.poll();
-            if(cur[0] == n-1 && cur[1] == m-1){
-                return cur[2];
+            if(cur[0]==n&&cur[1]==m){
+                answer = Math.min(cur[2],answer);
+                continue;
             }
-            
             for(int i=0;i<4;i++){
                 int nx = cur[0]+dx[i];
                 int ny = cur[1]+dy[i];
-                int nd = cur[2]+1;
-                if(nx>=0&&nx<n&&ny>=0&&ny<m){
-                    if(!v[nx][ny]&&maps[nx][ny]==1){
-                        q.add(new int[]{nx,ny,nd});
-                        v[nx][ny] = true;
-                    }
+                if(nx<0||nx>n||ny<0||ny>m) continue;
+                if(maps[nx][ny]==1&&!v[nx][ny]){
+                    q.add(new int[]{nx,ny,cur[2]+1});
+                    v[nx][ny] = true;
                 }
+                    
             }
         }
-        return answer;
+        return answer==Integer.MAX_VALUE?-1:answer;
     }
 }
