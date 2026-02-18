658df0a2d8d92a657a985cffda40dc20af249536
ssooomm <101492513+ssooomm@users.noreply.github.com>
2026-02-19 00:05:05 +0900

    [level 2] Title: 피로도, Time: 3.01 ms, Memory: 69.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,47 +1,24 @@
-import java.util.*;
-
 class Solution {
+    static int ans=0;
     public int solution(int k, int[][] dungeons) {
-        int answer = 0;
         int len = dungeons.length;
-        List<List<Integer>> res = new ArrayList<>();
-        boolean[] visited = new boolean[len+1];
-        backtrack(len,visited,new ArrayList<>(), res);
-        
-        int max = 0;
-        for(List<Integer> l:res){
-            int tmp = k;
-            int cnt = 0;
-            for(int n: l){
-                if(tmp>=dungeons[n-1][0]){
-                    tmp-=dungeons[n-1][1];
-                    cnt++;
-                }
-                else{
-                    break;
-                }
-            }
-            if(max<cnt){
-                max = cnt;
-            }
-        }
-
-        return max;
+        bt(dungeons,k,0,new boolean[len]);
+        return ans;
     }
-    void backtrack(int n, boolean[] visited, List<Integer> cur, List<List<Integer>> res){
-        if(cur.size() == n){
-            res.add(new ArrayList<>(cur));
-            return;
-        }
-        
-        for(int i=1;i<=n;i++){
-            if(!visited[i]){
-                visited[i] = true;
-                cur.add(i);
-                backtrack(n,visited, cur, res);
-                cur.remove(cur.size()-1);
-                visited[i] = false;
+    
+    void bt(int[][] dungeons,int cur,int cnt,boolean[] v){
+        for(int i=0;i<dungeons.length;i++){
+            if(!v[i]){
+                int[] tmp = dungeons[i];
+                if(cur<tmp[0]){
+                    continue;
+                }
+                v[i] = true;
+                bt(dungeons,cur-tmp[1],cnt+1,v);
+                v[i] = false;
             }
         }
+        ans = Math.max(ans,cnt);
+        return;
     }
 }
