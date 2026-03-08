341cc8174282b4a4c9aa13fd6ca9538c743c6361
ssooomm <101492513+ssooomm@users.noreply.github.com>
2026-03-08 11:31:11 +0900

    [Silver III] Title: 수열, Time: 240 ms, Memory: 23864 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -1,23 +1,26 @@
 import java.util.*;
 import java.io.*;
 
-class Main {
-    public static void main(String[] args) throws IOException{
+public class Main{
+    public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
         int[] arr = new int[n];
         st = new StringTokenizer(br.readLine());
-        int sum =0;
         for(int i=0;i<n;i++){
             arr[i] = Integer.parseInt(st.nextToken());
-            if(i<k) sum+=arr[i];
         }
-        int max = sum;
-        for(int i=k;i<n;i++){
-            sum+=arr[i]-arr[i-k];
-            max = Math.max(sum,max);
+        
+        int bef=0;
+        for(int i=0;i<k;i++){
+            bef+=arr[i];
+        }
+        int max = bef;
+        for(int i=0;i<n-k;i++){
+            bef-=arr[i]-arr[i+k];
+            max = Math.max(bef,max);
         }
         System.out.println(max);
     }
