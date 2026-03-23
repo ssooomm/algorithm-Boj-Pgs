31bbafc47e57cc31e4c480ce05590716b7f1c6d2
ssooomm <101492513+ssooomm@users.noreply.github.com>
2026-03-24 03:16:46 +0900

    [level 2] Title: 호텔 대실, Time: 10.29 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -2,40 +2,45 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] book_time) {
-        Arrays.sort(book_time,(x,y)->x[0].compareTo(y[0]));
-//        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b)->a[1]-b[1]); //대실시간이 가장짧은 거 먼저
-
-        int[][] time = new int[book_time.length][2];
-        for(int i=0;i<book_time.length;i++){
-            int start_time = Integer.parseInt(book_time[i][0].replace(":",""));
-            int end_time = Integer.parseInt(book_time[i][1].replace(":",""));
+        int answer = 0;
+        Queue<int[]> q = new PriorityQueue<>((a,b)->{
+            if(a[0]==b[0]) return a[1]-b[1];
+            else return a[0]-b[0];
+        });
+        
+        for(String[] book:book_time){
+            int[] tmp = new int[2];
             
-            end_time+=10;
-            if(end_time%100>=60){
-                end_time+=40;
-            }
-            time[i][0]=start_time;
-            time[i][1]=end_time;
+            String[] time = book[0].split(":");
+            int h = Integer.parseInt(time[0]);
+            tmp[0] = h*60+Integer.parseInt(time[1]);
+
+            time = book[1].split(":");
+            h = Integer.parseInt(time[0]);
+            tmp[1] = h*60+Integer.parseInt(time[1])+10; 
+            
+            q.add(tmp);
         }
-
-        ArrayList<Integer> rooms = new ArrayList<>();
-        for(int i=0;i<time.length;i++){
-            Collections.sort(rooms);
-            
-            boolean isAdd=false;
-            
-            for(int j=0;j<rooms.size();j++){
-                if(time[i][0] >= rooms.get(j)){
-                    rooms.set(j,time[i][1]);
-                    isAdd=true;
+        
+        int[] arr = new int[1000];
+        Arrays.fill(arr,-1);
+        arr[0] = q.poll()[1];
+        
+        while(!q.isEmpty()){
+            int[] tmp = q.poll();
+            for(int i=0;i<1000;i++){
+                if(arr[i]==-1||arr[i]<=tmp[0]){
+                    arr[i] = tmp[1];
                     break;
                 }
-            }
-            
-            if(!isAdd){
-                rooms.add(time[i][1]);
+                
             }
         }
-        return rooms.size();
+        
+        for(int i=0;i<1000;i++){
+            if(arr[i]!=-1) answer++;
+            else if(arr[i]==-1) break;
+        }
+        return answer;
     }
 }
