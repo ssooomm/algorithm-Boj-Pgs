4aaeab2b30e8385cb62db7efa6c0380a662647bf
ssooomm <101492513+ssooomm@users.noreply.github.com>
2026-04-08 00:43:56 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.26 ms, Memory: 89.8 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,50 +1,49 @@
 import java.util.*;
 
 class Solution {
-    static int[] dx = {1,-1,0,0};
-    static int[] dy = {0,0,1,-1};
+    static int[] dx = new int[]{0,0,1,-1};
+    static int[] dy = new int[]{1,-1,0,0};
+    
     public int[] solution(String[][] places) {
-        int[] answer = new int[5];
-        
+        int[] ans = new int[5];
         for(int i=0;i<5;i++){
-            answer[i] = check(places[i]);
+            if(fun(places[i])) ans[i] = 1;
         }
-        return answer;
-        
+        return ans;
     }
     
-    int check(String[] place){
-        for(int i=0;i<5;i++){
-            for(int j=0;j<5;j++){
-                if(place[i].charAt(j)=='P'){
-                    if(!bfs(i,j,place)) return 0;
+    public boolean fun(String[] place){
+        for(int j=0;j<5;j++){
+            for(int k=0;k<5;k++){
+                char ch = place[j].charAt(k);
+                if(ch=='P'){
+                    if(!bfs(place,j,k)) return false;
                 }
             }
         }
-        return 1;
+        return true;
     }
     
-    boolean bfs(int r, int c, String[] place){
+    public boolean bfs(String[] place, int x, int y){
+        boolean[][] v = new boolean[5][5];
         Queue<int[]> q = new ArrayDeque<>();
-        boolean[][] v= new boolean[5][5];
-        q.add(new int[]{r,c,0});
-        v[r][c] = true;
+        q.add(new int[]{x,y,0});
+        v[x][y] = true;
         
         while(!q.isEmpty()){
             int[] cur = q.poll();
-            v[cur[0]][cur[1]] = true;
-            if(cur[2]>=2) continue;
-            
             for(int i=0;i<4;i++){
                 int nx = cur[0]+dx[i];
                 int ny = cur[1]+dy[i];
                 int nd = cur[2]+1;
-                if(nx>=0&&nx<5&&ny>=0&&ny<5){
-                    if(!v[nx][ny]&&place[nx].charAt(ny)!='X'){
-                        if(place[nx].charAt(ny)=='P') return false;
-                        q.add(new int[]{nx,ny,nd});
-                        
+                if(nx<0||nx>=5||ny<0||ny>=5) continue;
+                char ch = place[nx].charAt(ny);
+                if(!v[nx][ny]&&ch!='X'){
+                    if(ch=='P'){
+                        if(nd<=2) return false;
                     }
+                    v[nx][ny] = true;
+                    q.add(new int[]{nx,ny,nd});
                 }
             }
         }
