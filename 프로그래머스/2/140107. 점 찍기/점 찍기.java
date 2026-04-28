327573c96966abaafcfc2f097c43344fb46fe41f
ssooomm <101492513+ssooomm@users.noreply.github.com>
2026-04-28 23:49:00 +0900

    [level 2] Title: 점 찍기, Time: 15.23 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.java
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -1,14 +1,16 @@
 import java.util.*;
+
 class Solution {
     public long solution(int k, int d) {
         long answer = 0;
-        long d2 = (long)Math.pow(d,2);
         
-        for (int i = 0; i <= d; i+=k) {
-            long i2 = (long)Math.pow(i,2);
-            int a = (int)Math.sqrt(d2-i2);
-            answer += (a/k) + 1;
+        long dd = (long) d*d;
+        
+        for(long x=0;x<=d;x+=k){
+            long y = (long)Math.sqrt(dd - (x*x));
+            answer+=(y/k)+1; //k개 차이로 나올 수 있는 y값 개수 + 0값
         }
+
         return answer;
     }
 }
