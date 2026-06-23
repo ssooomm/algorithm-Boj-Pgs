3022bc11e1a45b3a4f8f2c275fa32f0cfa4dbb08
ssooomm <101492513+ssooomm@users.noreply.github.com>
2026-06-23 19:00:46 +0900

    [level 2] Title: N-Queen, Time: 151.84 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -1,23 +1,30 @@
 class Solution {
-
     public int solution(int n) {
         boolean[][] board = new boolean[n][n];
-        return place(board,0,n);
         
+        return check(board,0,n);
     }
-    public int place(boolean[][] board, int r, int n){
-        if(r==n) return 1;
+    
+    int check(boolean[][] board,int r,int n){
+        
+        if(r==n){
+            return 1;
+        }
+        
         int cnt=0;
         for(int c=0;c<n;c++){
-            if(!board[r][c] && check(board,r,c,n)){
+            if(isValid(board,r,c,n)){
                 board[r][c] = true;
-                cnt+=place(board,r+1,n);
+                cnt+=check(board,r+1,n);
                 board[r][c] = false;
             }
         }
+
         return cnt;
     }
-    public boolean check(boolean[][] board, int r, int c, int n){
+
+    
+    boolean isValid(boolean[][] board, int r, int c, int n){
         for(int i=0;i<r;i++){
             if(board[i][c]) return false;
         }
