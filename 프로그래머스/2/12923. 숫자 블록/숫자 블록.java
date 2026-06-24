b8f892a5a8e93f0a3e9e0964e1cd97ab78a0c013
ssooomm <101492513+ssooomm@users.noreply.github.com>
2026-06-24 19:55:34 +0900

    [level 2] Title: 숫자 블록, Time: 59.51 ms, Memory: 58.6 MB -BaekjoonHub

--- 프로그래머스/2/12923. 숫자 블록/숫자 블록.java
+++ 프로그래머스/2/12923. 숫자 블록/숫자 블록.java
@@ -1,29 +1,30 @@
 class Solution {
     public int[] solution(long begin, long end) {
-        int n = (int)(end - begin + 1);
+        int n = (int)(end-begin+1);
         int[] answer = new int[n];
         
-        for(int i = 0; i < n; i++){
-            answer[i] = get((int)(begin + i));
+        for(int i=0;i<n;i++){
+            answer[i] = get((int)(begin+i));
         }
 
         return answer;
     }
     
     int get(int n){
-        if(n == 1) return 0;
+        if(n==1) return 0;
         
-        int ans = 1; // 약수를 못 찾으면 기본값은 1 (소수 케이스)
+        int ans = 1;
         
-        for(int i = 2; i <= Math.sqrt(n); i++){
-            if(n % i == 0){ // 나누어 떨어지는 '진짜 약수'일 때만!
-                if(n / i <= 10000000) {
-                    return n / i; 
-                }
-                ans = i; // n/i가 1000만을 넘어가면 짝꿍인 i를 정답 후보로 백업
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(n%i==0){
+                if(n/i<=10000000) return n/i;
+                
+                ans = i;
             }
+            
         }
         
-        return ans; 
+        return ans; //소수
     }
+    
 }
