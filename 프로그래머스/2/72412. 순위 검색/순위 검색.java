a6820e548c1b4b2fabe92c5516f0a5894ec7893f
ssooomm <101492513+ssooomm@users.noreply.github.com>
2026-07-01 16:35:06 +0900

    [level 2] Title: 순위 검색, Time: 486.00 ms, Memory: 210 MB -BaekjoonHub

--- 프로그래머스/2/72412. 순위 검색/순위 검색.java
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.java
@@ -1,71 +1,52 @@
 import java.util.*;
 
 class Solution {
-    
-    //학생 당 16가지 조건씩, 앞에 4가지 일치하는 학생들의 점수만 저장
-    static Map<String,List<Integer>> map = new HashMap<>();
+    class Node{
+        String lang;
+        String pos;
+        String career;
+        String food;
+        int score;
+        
+        public Node(String lang, String pos, String career, String food, int score) {
+            this.lang = lang;
+            this.pos = pos;
+            this.career = career;
+            this.food = food;
+            this.score = score;
+        }
+    }
     
     public int[] solution(String[] info, String[] query) {
         int in = info.length;
         int qn = query.length;
         int[] answer = new int[qn];
         
-        //사람당 가질 수 있는 16가지 조합 저장
+        List<Node> list = new ArrayList<>();
+
         for(String i:info){
             String[] arr = i.split(" ");
-            int score = Integer.parseInt(arr[4]);
-            make(arr,"",0,score);
-        }
-        
-        //점수 정렬
-        for(String k:map.keySet()){
-            Collections.sort(map.get(k));
+            list.add(new Node(arr[0],arr[1],arr[2],arr[3],Integer.parseInt(arr[4])));
         }
         
         for(int i=0;i<qn;i++){
-            String q = query[i].replace(" and ",""); //map에 저장된 k값 처럼 문자열 생김
-            String[] arr = q.split(" ");
-            
-            String key = arr[0];
-            int target = Integer.parseInt(arr[1]);
-            
-            if(!map.containsKey(key)){
-                answer[i] = 0;
-                continue;
+            int cnt=0;
+            String[] arr = query[i].split(" ");
+            for(int j=0;j<in;j++){
+                
+                Node n = list.get(j);
+                
+                if(!arr[0].equals(n.lang)&&!arr[0].equals("-")) continue;
+                if(!arr[2].equals(n.pos)&&!arr[2].equals("-")) continue;
+                if(!arr[4].equals(n.career)&&!arr[4].equals("-")) continue;
+                if(!arr[6].equals(n.food)&&!arr[6].equals("-")) continue;
+                if(Integer.parseInt(arr[7])>n.score) continue;
+                
+                cnt++;       
             }
             
-            //이진탐색
-            List<Integer> list = map.get(key);
-            answer[i] = list.size()-binary(list,target);
+            answer[i] = cnt;
         }
-        
         return answer;
-    }
-    
-    public void make(String[] arr, String str, int depth, int score){
-        if(depth==4){
-            map.computeIfAbsent(str,k->new ArrayList<>()).add(score);
-            //없으면 계산해서 map에 넣어라 -> 해당 k가진 map만들어지고 객체 생성해서 점수 들어감, 이미 k있으면 무시
-            return;
-        }
-        //원본 조건
-        make(arr,str+arr[depth],depth+1,score);
-        //하이픈
-        make(arr,str+"-",depth+1,score);
-    }
-    
-    public int binary(List<Integer> list, int target){
-        int l = 0;
-        int r = list.size();
-        
-        while(l<r){
-            int mid = (l+r)/2;
-            if(list.get(mid)>=target){
-                r = mid;
-            }else
-                l = mid+1;
-        }
-        
-        return l;
     }
 }
